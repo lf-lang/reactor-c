09ae0182e16bb079b98916923d2736cd9d4b943f
eal <eal@eecs.berkeley.edu>
2021-10-20 07:12:41 -0700

    Unlock mutex when registering a trace event fails.

--- core/trace.c
+++ core/trace.c
@@ -90,6 +90,7 @@ int _lf_trace_object_descriptions_size = 0;
 int _lf_register_trace_event(void* pointer1, void* pointer2, _lf_trace_object_t type, char* description) {
     lf_mutex_lock(&_lf_trace_mutex);
     if (_lf_trace_object_descriptions_size >= TRACE_OBJECT_TABLE_SIZE) {
+        lf_mutex_unlock(&_lf_trace_mutex);
         return 0;
     }
     _lf_trace_object_descriptions[_lf_trace_object_descriptions_size].pointer = pointer1;
