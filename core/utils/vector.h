ca240efbf4f8f57d970c13cb70058148cf8be024
Soroush Bateni <soroosh129@gmail.com>
2021-12-10 13:27:14 -0600

    Updated vector to use realloc

--- core/utils/vector.h
+++ core/utils/vector.h
@@ -18,19 +18,20 @@ typedef struct vector_t {
     void** end;   /* The end of the underlying array. */
 } vector_t;
 
-/*
+/**
  * Allocate and initialize a new vector.
  * @param initial_capacity The desired initial capacity to allocate.
+ *  Must be more than 0
  */
 vector_t vector_new(size_t initial_capacity);
 
-/*
+/**
  * Free the memory held by the given vector, invalidating it.
  * @param v Any vector.
  */
 void vector_free(vector_t* v);
 
-/*
+/**
  * Add the given element to the vector. The given element should be
  * non-null.
  * @param v A vector that is to grow.
@@ -38,7 +39,7 @@ void vector_free(vector_t* v);
  */
 void vector_push(vector_t* v, void* element);
 
-/*
+/**
  * Add all elements of the given array to the vector. Elements should be
  * non-null.
  * @param v A vector that is to grow.
@@ -47,14 +48,14 @@ void vector_push(vector_t* v, void* element);
  */
 void vector_pushall(vector_t* v, void** array, size_t size);
 
-/*
+/**
  * Remove and return some pointer that is contained in the given vector,
  * or return NULL if the given vector is empty.
  * @param v Any vector.
  */
 void* vector_pop(vector_t* v);
 
-/*
+/**
  * Vote on whether this vector ought to have a smaller memory footprint.
  */
 void vector_vote(vector_t* v);
