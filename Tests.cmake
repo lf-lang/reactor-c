fa9581e2895e9c27ad77e1ddc237ce55326b13dc
Peter Donovan <peterdonovan@berkeley.edu>
2021-12-17 15:42:50 -0800

    Clean up includes for tests.

--- Tests.cmake
+++ Tests.cmake
@@ -1,15 +1,19 @@
 # This adds all tests in the test directory.
 include(CTest)
+set(TestLib test-lib)
 set(TEST_DIR ${CMAKE_CURRENT_SOURCE_DIR}/test)
 file(
     GLOB_RECURSE TEST_FILES
     LIST_DIRECTORIES false
     RELATIVE ${TEST_DIR}
-    ${TEST_DIR}/*.c
+    ${TEST_DIR}/*test.c
 )
 foreach(FILE ${TEST_FILES})
     string(REGEX REPLACE "[./]" "_" NAME ${FILE})
     add_executable(${NAME} ${TEST_DIR}/${FILE})
     add_test(NAME ${NAME} COMMAND ${NAME})
-    target_link_libraries(${NAME} PUBLIC ${CoreLib} ${PlatformLib} ${Lib})
+    target_link_libraries(
+        ${NAME} PUBLIC
+        ${CoreLib} ${PlatformLib} ${Lib} ${TestLib}
+    )
 endforeach(FILE ${TEST_FILES})
--- test/tag_test.c
+++ test/tag_test.c
@@ -1,14 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "../core/reactor.h"
-
-void _lf_initialize_trigger_objects() {}
-void terminate_execution() {}
-bool _lf_trigger_shutdown_reactions() { return true; }
-void _lf_set_default_command_line_options() {}
-void _lf_trigger_startup_reactions() {}
-void _lf_initialize_timers() {}
-
+#include "core/reactor.h"
 
 int main(int argc, char **argv) {
   char* buf = malloc(sizeof(char) * 128);
