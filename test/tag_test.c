f58db8ee4468abcb2a4d233f58fe048b48a5a0d9
Peter Donovan <peterdonovan@berkeley.edu>
2021-12-16 22:03:41 -0800

    Ugly but functional first pass to make unit test work.

--- test/tag_test.c
+++ test/tag_test.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "tag.h"
-#include "reactor.h"
+#include "../core/reactor.h"
 
-void _lf_initialize_timers() {}
-void logical_tag_complete(tag_t tag_to_send) {}
-void terminate_execution() {}
-void _lf_trigger_startup_reactions() {}
 void _lf_initialize_trigger_objects() {}
+void terminate_execution() {}
 bool _lf_trigger_shutdown_reactions() { return true; }
+void _lf_set_default_command_line_options() {}
+void _lf_trigger_startup_reactions() {}
+void _lf_initialize_timers() {}
+
 
 int main(int argc, char **argv) {
   char* buf = malloc(sizeof(char) * 128);
