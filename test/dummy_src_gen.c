60243d59f6f4a1470f01bdeaa8017169d9dbbe84
Peter Donovan <peterdonovan@berkeley.edu>
2021-12-17 15:42:50 -0800

    Clean up includes for tests.

---
+++ test/dummy_src_gen.c
@@ -0,0 +1,8 @@
+#include <stdbool.h>
+
+void _lf_initialize_trigger_objects() {}
+void terminate_execution() {}
+bool _lf_trigger_shutdown_reactions() { return true; }
+void _lf_set_default_command_line_options() {}
+void _lf_trigger_startup_reactions() {}
+void _lf_initialize_timers() {}
