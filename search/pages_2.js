7fe08101133c926459d1e6cc7dba86b5a7833b11
lhstrh <marten@berkeley.edu>
2022-01-04 08:08:14 +0000

    Deploying to gh-pages from  @ b2b26b9604cd3f54ddd7a2ba3c90c0fe9fdcc3ec 🚀

--- search/pages_2.js
+++ search/pages_2.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['reactor_2dc_3a_20a_20reactor_20runtime_20implementation_20in_20c_0',['Reactor-C: A reactor runtime implementation in C',['../md__r_e_a_d_m_e.html',1,'']]],
-  ['readme_1',['README',['../md_core_federated__r_t_i__r_e_a_d_m_e.html',1,'']]]
+  ['readme_0',['README',['../md_core_federated__r_t_i__r_e_a_d_m_e.html',1,'(Global Namespace)'],['../md__r_e_a_d_m_e.html',1,'(Global Namespace)']]]
 ];
