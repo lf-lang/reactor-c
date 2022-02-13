a0d1f09b2935b1f698117ef2fe9cf68b06b88dd9
hokeun <hokeunkim@berkeley.edu>
2022-02-13 01:53:00 +0000

    Deploying to gh-pages from  @ 89dd92632e6963f8a3b298710740ee90124c5cf1 🚀

--- search/functions_2.js
+++ search/functions_2.js
@@ -2,15 +2,16 @@ var searchData=
 [
   ['calculate_5fepoch_5foffset_0',['calculate_epoch_offset',['../lf__unix__clock__support_8c.html#a1b0a8eaaff75fcae37d968ca5de88702',1,'lf_unix_clock_support.c']]],
   ['callback_1',['callback',['../audio__loop__linux_8c.html#a0aaba34d923e5940eba8f2857bd60ad4',1,'callback(snd_pcm_t *playback_handle, int16_t buf_ref[]):&#160;audio_loop_linux.c'],['../audio__loop__mac_8c.html#a84211de54f3d6c0fa430f5a31291fcd0',1,'callback(void *ignored, AudioQueueRef queue, AudioQueueBufferRef buf_ref):&#160;audio_loop_mac.c']]],
-  ['clock_5fsynchronization_5fthread_2',['clock_synchronization_thread',['../rti_8c.html#a272b481a0cc2f86f21c75e8efa19a551',1,'rti.c']]],
-  ['compare_5ftags_3',['compare_tags',['../tag_8c.html#a6f9d6db9250698b048f5f118b0308e47',1,'compare_tags(tag_t tag1, tag_t tag2):&#160;tag.c'],['../tag_8h.html#a6f9d6db9250698b048f5f118b0308e47',1,'compare_tags(tag_t tag1, tag_t tag2):&#160;tag.c']]],
-  ['connect_5fto_5ffederate_4',['connect_to_federate',['../federate_8c.html#a2ca619f14cb3f3e777c8ca2e0e9ec022',1,'federate.c']]],
-  ['connect_5fto_5ffederates_5',['connect_to_federates',['../rti_8c.html#acf8907ccab075522f7b84de579d50030',1,'rti.c']]],
-  ['connect_5fto_5frti_6',['connect_to_rti',['../federate_8c.html#ac4fb5cd6e02f70979dd3e30310d4b2bb',1,'federate.c']]],
-  ['convert_5fns_5fto_5ftimespec_7',['convert_ns_to_timespec',['../lf__unix__clock__support_8c.html#af509cb4725e09a3e9af8d84bc98e4331',1,'lf_unix_clock_support.c']]],
-  ['convert_5ftimespec_5fto_5fns_8',['convert_timespec_to_ns',['../lf__unix__clock__support_8c.html#ab5a60065ba9bd0644386acf7f8c5e1d2',1,'lf_unix_clock_support.c']]],
-  ['convert_5fvolatile_5ftag_5fto_5fnonvolatile_9',['convert_volatile_tag_to_nonvolatile',['../tag_8c.html#a3dc06a20d8902397916c0784e3a956c7',1,'convert_volatile_tag_to_nonvolatile(tag_t volatile vtag):&#160;tag.c'],['../tag_8h.html#a3dc06a20d8902397916c0784e3a956c7',1,'convert_volatile_tag_to_nonvolatile(tag_t volatile vtag):&#160;tag.c']]],
-  ['create_5fclock_5fsync_5fthread_10',['create_clock_sync_thread',['../clock-sync_8c.html#ac094b53ced87d3cbd617a66591f4282a',1,'create_clock_sync_thread(lf_thread_t *thread_id):&#160;clock-sync.c'],['../clock-sync_8h.html#ac094b53ced87d3cbd617a66591f4282a',1,'create_clock_sync_thread(lf_thread_t *thread_id):&#160;clock-sync.c']]],
-  ['create_5fserver_11',['create_server',['../federate_8c.html#a0e2425b519aae66167fbf454f896df92',1,'create_server(int specified_port):&#160;federate.c'],['../rti_8c.html#acde4076cd96817debe76e74fca261c70',1,'create_server(int32_t specified_port, uint16_t port, socket_type_t socket_type):&#160;rti.c']]],
-  ['create_5ftoken_12',['create_token',['../reactor_8h.html#a0b3e914b48f5f90533eecbf63acdf8d9',1,'create_token(size_t element_size):&#160;reactor_common.c'],['../reactor__common_8c.html#a0b3e914b48f5f90533eecbf63acdf8d9',1,'create_token(size_t element_size):&#160;reactor_common.c']]]
+  ['check_5fdeadline_2',['check_deadline',['../ctarget_8h.html#af7f583b9672725a4c06b4c5612dae012',1,'check_deadline(void *self):&#160;ctarget.c'],['../ctarget_8c.html#af7f583b9672725a4c06b4c5612dae012',1,'check_deadline(void *self):&#160;ctarget.c']]],
+  ['clock_5fsynchronization_5fthread_3',['clock_synchronization_thread',['../rti_8c.html#a272b481a0cc2f86f21c75e8efa19a551',1,'rti.c']]],
+  ['compare_5ftags_4',['compare_tags',['../tag_8c.html#a6f9d6db9250698b048f5f118b0308e47',1,'compare_tags(tag_t tag1, tag_t tag2):&#160;tag.c'],['../tag_8h.html#a6f9d6db9250698b048f5f118b0308e47',1,'compare_tags(tag_t tag1, tag_t tag2):&#160;tag.c']]],
+  ['connect_5fto_5ffederate_5',['connect_to_federate',['../federate_8c.html#a2ca619f14cb3f3e777c8ca2e0e9ec022',1,'federate.c']]],
+  ['connect_5fto_5ffederates_6',['connect_to_federates',['../rti_8c.html#acf8907ccab075522f7b84de579d50030',1,'rti.c']]],
+  ['connect_5fto_5frti_7',['connect_to_rti',['../federate_8c.html#ac4fb5cd6e02f70979dd3e30310d4b2bb',1,'federate.c']]],
+  ['convert_5fns_5fto_5ftimespec_8',['convert_ns_to_timespec',['../lf__unix__clock__support_8c.html#af509cb4725e09a3e9af8d84bc98e4331',1,'lf_unix_clock_support.c']]],
+  ['convert_5ftimespec_5fto_5fns_9',['convert_timespec_to_ns',['../lf__unix__clock__support_8c.html#ab5a60065ba9bd0644386acf7f8c5e1d2',1,'lf_unix_clock_support.c']]],
+  ['convert_5fvolatile_5ftag_5fto_5fnonvolatile_10',['convert_volatile_tag_to_nonvolatile',['../tag_8c.html#a3dc06a20d8902397916c0784e3a956c7',1,'convert_volatile_tag_to_nonvolatile(tag_t volatile vtag):&#160;tag.c'],['../tag_8h.html#a3dc06a20d8902397916c0784e3a956c7',1,'convert_volatile_tag_to_nonvolatile(tag_t volatile vtag):&#160;tag.c']]],
+  ['create_5fclock_5fsync_5fthread_11',['create_clock_sync_thread',['../clock-sync_8c.html#ac094b53ced87d3cbd617a66591f4282a',1,'create_clock_sync_thread(lf_thread_t *thread_id):&#160;clock-sync.c'],['../clock-sync_8h.html#ac094b53ced87d3cbd617a66591f4282a',1,'create_clock_sync_thread(lf_thread_t *thread_id):&#160;clock-sync.c']]],
+  ['create_5fserver_12',['create_server',['../federate_8c.html#a0e2425b519aae66167fbf454f896df92',1,'create_server(int specified_port):&#160;federate.c'],['../rti_8c.html#acde4076cd96817debe76e74fca261c70',1,'create_server(int32_t specified_port, uint16_t port, socket_type_t socket_type):&#160;rti.c']]],
+  ['create_5ftoken_13',['create_token',['../reactor_8h.html#a0b3e914b48f5f90533eecbf63acdf8d9',1,'create_token(size_t element_size):&#160;reactor_common.c'],['../reactor__common_8c.html#a0b3e914b48f5f90533eecbf63acdf8d9',1,'create_token(size_t element_size):&#160;reactor_common.c']]]
 ];
