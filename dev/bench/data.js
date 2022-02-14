5bc349c1e78c3ca5bd227690f742c0dea67df215
github-action-benchmark <github@users.noreply.github.com>
2022-02-14 18:31:32 +0000

    add Lingua Franca C target Benchmark (customSmallerIsBetter) benchmark result for 81b4582fc1fa045ee5cdb10c3584c3346ce86105

---
+++ dev/bench/data.js
@@ -0,0 +1,44 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1644863492745,
+  "repoUrl": "https://github.com/lf-lang/reactor-c",
+  "entries": {
+    "Lingua Franca C target Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "arthurdeng0205@hotmail.com",
+            "name": "LDeng0205",
+            "username": "LDeng0205"
+          },
+          "committer": {
+            "email": "arthurdeng0205@hotmail.com",
+            "name": "LDeng0205",
+            "username": "LDeng0205"
+          },
+          "distinct": true,
+          "id": "81b4582fc1fa045ee5cdb10c3584c3346ce86105",
+          "message": "adjusted workflow settings",
+          "timestamp": "2022-02-14T10:28:03-08:00",
+          "tree_id": "fa03ff0f4b16f64732ca9a358330a80189dc73df",
+          "url": "https://github.com/lf-lang/reactor-c/commit/81b4582fc1fa045ee5cdb10c3584c3346ce86105"
+        },
+        "date": 1644863491076,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Ping Pong",
+            "value": 346.162318,
+            "unit": "ms",
+            "extra": "Target: lf-c\nTotal Iterations: 1\nThreads: 2"
+          },
+          {
+            "name": "Counting Actor",
+            "value": 342.313648,
+            "unit": "ms",
+            "extra": "Target: lf-c\nTotal Iterations: 1\nThreads: 2"
+          }
+        ]
+      }
+    ]
+  }
+}
