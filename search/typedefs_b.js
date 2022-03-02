a28bdf84bb059ca2a2a8c15a0e38da8246c9626d
edwardalee <eal@eecs.berkeley.edu>
2022-03-02 00:21:33 +0000

    Deploying to gh-pages from  @ d2d908bc2db10ffb60860ad42f482956ffac5abf 🚀

--- search/typedefs_b.js
+++ search/typedefs_b.js
@@ -2,6 +2,7 @@ var searchData=
 [
   ['reaction_5ffunction_5ft_0',['reaction_function_t',['../reactor_8h.html#a149e5fee1c1841bcc96c72f200601d90',1,'reactor.h']]],
   ['reaction_5ft_1',['reaction_t',['../reactor_8h.html#a1f0f71b6c8e0c54c65cbfaa154b40694',1,'reactor.h']]],
-  ['rti_5faddr_5finfo_5ft_2',['rti_addr_info_t',['../net__util_8h.html#af820f39ab52ce0a58d7ba739051b8f24',1,'net_util.h']]],
-  ['rti_5finstance_5ft_3',['RTI_instance_t',['../rti_8h.html#a97b6813c17e8a4fa159935a5c2915d28',1,'rti.h']]]
+  ['reactor_5fmode_5ft_2',['reactor_mode_t',['../reactor_8h.html#a17df34d9c9d092aa6a399512b6a023e4',1,'reactor.h']]],
+  ['rti_5faddr_5finfo_5ft_3',['rti_addr_info_t',['../net__util_8h.html#af820f39ab52ce0a58d7ba739051b8f24',1,'net_util.h']]],
+  ['rti_5finstance_5ft_4',['RTI_instance_t',['../rti_8h.html#a97b6813c17e8a4fa159935a5c2915d28',1,'rti.h']]]
 ];
