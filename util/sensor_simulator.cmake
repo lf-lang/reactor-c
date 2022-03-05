3d7c4f10f4a93379b5a550639ab3ea836a38835f
eal <eal@eecs.berkeley.edu>
2022-03-04 17:35:12 -0800

    Added CMake requirements for curses to the sensor simulator cmake file

--- util/sensor_simulator.cmake
+++ util/sensor_simulator.cmake
@@ -1,1 +1,4 @@
 target_sources(${LF_MAIN_TARGET} PRIVATE sensor_simulator.c)
+find_package(Curses REQUIRED) # Finds the lncurses library
+include_directories(${CURSES_INCLUDE_DIR}) # "The include directories needed to use Curses"
+target_link_libraries( ${LF_MAIN_TARGET} ${CURSES_LIBRARIES} ) # Links the Curses library
