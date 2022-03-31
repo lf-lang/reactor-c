48307021c15b9beb7062654beea8be72772a8836
Peter Donovan <peterdonovan@berkeley.edu>
2022-03-31 16:24:03 -0700

    [scheduler] Adjust data_collection.h.
    If I am right, expected trials required to go from an intermediate number of workers to an extreme (1 or the maximum) number of workers was previously quadtratic in # of workers (cuz random walk) -- no good. This should make it (log(n))^2.

--- core/threaded/data_collection.h
+++ core/threaded/data_collection.h
@@ -15,6 +15,11 @@ static interval_t* execution_times_mins;
 static size_t* execution_times_argmins;
 static size_t data_collection_counter = 0;
 static bool collecting_data = false;
+/**
+ * A monotonically increasing sequence of numbers of workers, the first and last elements of which
+ * are too large or small to be realizable.
+ */
+static size_t* possible_nums_workers;
 
 extern size_t num_levels;
 extern size_t max_num_workers;
@@ -23,6 +28,33 @@ extern size_t max_num_workers;
 #define PARALLELISM_COST 20000
 #define EXECUTION_TIME_MEMORY 15
 
+static void possible_nums_workers_init() {
+    // Start with 0 and end with a number strictly greater than max_num_workers.
+    size_t pnw_length = 2;
+    size_t temp = max_num_workers;
+    while ((temp >>= 1)) pnw_length++;
+    possible_nums_workers = malloc(pnw_length * sizeof(size_t));
+    temp = 1;
+    possible_nums_workers[0] = 0;
+    for (int i = 1; i < pnw_length; i++) {
+        possible_nums_workers[i] = temp;
+        temp *= 2;
+    }
+    assert(temp > max_num_workers);
+}
+
+/** Get the result of a state transition. */
+static size_t get_nums_workers_neighboring_state(size_t current_state) {
+    // TODO: There is a more efficient way to do this. However, this operation should be uncommon
+    // asymptotically.
+    size_t range = 1;
+    size_t jitter = ((int) (rand() % (2 * range + 1))) - range;
+    if (!jitter) return current_state;
+    size_t i = 1;
+    while (possible_nums_workers[i] < current_state) i++;
+    return possible_nums_workers[i + jitter];
+}
+
 static void data_collection_init(sched_params_t* params) {
     size_t num_levels = params->num_reactions_per_level_size;
     start_times_by_level = (interval_t*) calloc(num_levels, sizeof(interval_t));
@@ -30,13 +62,14 @@ static void data_collection_init(sched_params_t* params) {
         num_levels, sizeof(interval_t*)
     );
     execution_times_mins = (interval_t*) calloc(num_levels, sizeof(interval_t));
-    execution_times_argmins = (size_t*) malloc(num_levels * sizeof(size_t));
+    execution_times_argmins = (size_t*) calloc(num_levels, sizeof(size_t));
     for (size_t i = 0; i < num_levels; i++) {
         execution_times_argmins[i] = max_num_workers;
         execution_times_by_num_workers_by_level[i] = (interval_t*) calloc(
             max_num_workers, sizeof(interval_t)
         ) - 1;
     }
+    possible_nums_workers_init();
 }
 
 static void data_collection_free() {
@@ -45,6 +78,7 @@ static void data_collection_free() {
         free(execution_times_by_num_workers_by_level[i] + 1);
     }
     free(execution_times_by_num_workers_by_level);
+    free(possible_nums_workers);
 }
 
 static void data_collection_start_level(size_t level) {
@@ -53,16 +87,15 @@ static void data_collection_start_level(size_t level) {
 
 static void data_collection_end_level(size_t level, size_t num_workers) {
     if (collecting_data && start_times_by_level[level]) {
+        interval_t dt = get_physical_time() - start_times_by_level[level];
         if (!execution_times_by_num_workers_by_level[level][num_workers]) {
-            execution_times_by_num_workers_by_level[level][num_workers]
-                = get_physical_time() - start_times_by_level[level];
-        } else {
-            interval_t prior_et = execution_times_by_num_workers_by_level[level][num_workers];
-            execution_times_by_num_workers_by_level[level][num_workers] = (
-                execution_times_by_num_workers_by_level[level][num_workers] * EXECUTION_TIME_MEMORY
-                + get_physical_time() - start_times_by_level[level]
-            ) / (EXECUTION_TIME_MEMORY + 1);
+            execution_times_by_num_workers_by_level[level][num_workers] = MAX(
+                dt,
+                2 * execution_times_by_num_workers_by_level[level][execution_times_argmins[level]]
+            );
         }
+        interval_t* prior_et = &execution_times_by_num_workers_by_level[level][num_workers];
+        *prior_et = (*prior_et * EXECUTION_TIME_MEMORY + dt) / (EXECUTION_TIME_MEMORY + 1);
     }
 }
 
@@ -89,7 +122,9 @@ static void compute_number_of_workers(
         size_t max_reasonable_num_workers = max_num_workers_by_level[level];
         ideal_number_of_workers = execution_times_argmins[level];
         int range = 1;
-        if (jitter) ideal_number_of_workers += ((int) (rand() % (2 * range + 1))) - range;
+        if (jitter) {
+            ideal_number_of_workers = get_nums_workers_neighboring_state(ideal_number_of_workers);
+        }
         num_workers_by_level[level] = restrict_to_range(
             1, max_reasonable_num_workers, ideal_number_of_workers
         );
