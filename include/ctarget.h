d85e9210d7e3e2213b2b17a9b1debe6ff43ae284
Steven <stevenhouseng@gmail.com>
2022-04-18 17:22:19 -0700

    add comments to user-facing SET_DESTRUCTOR and SET_COPY_CONSTRUCTOR

--- include/ctarget.h
+++ include/ctarget.h
@@ -145,9 +145,29 @@ THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 #define SET_TOKEN(out, newtoken) _LF_SET_TOKEN(out, newtoken)
 
+/**
+ * Set the destructor used to free "token->value" set on "out".
+ * That memory will be automatically freed once all downstream
+ * reactions no longer need the value.
+ * 
+ * @param out The output port (by name) or input of a contained
+ *            reactor in form input_name.port_name.
+ * @param dtor A pointer to a void function that takes a pointer argument
+ *             or NULL to use the default void free(void*) function. 
+ */
 #define SET_DESTRUCTOR(out, dtor) _LF_SET_DESTRUCTOR(out, dtor)
 
-#define SET_COPY_CONSTRUCTOR(in, cpy_ctor) _LF_SET_COPY_CONSTRUCTOR(in, cpy_ctor)
+
+/**
+ * Set the destructor used to copy construct "token->value" received
+ * by "in" if "in" is mutable.
+ * 
+ * @param out The output port (by name) or input of a contained
+ *            reactor in form input_name.port_name.
+ * @param cpy_ctor A pointer to a void* function that takes a pointer argument
+ *                 or NULL to use the memcpy operator.
+ */
+#define SET_COPY_CONSTRUCTOR(out, cpy_ctor) _LF_SET_COPY_CONSTRUCTOR(out, cpy_ctor)
 
 //////////////////////////////////////////////////////////////
 /////////////  SET_MODE Function (to switch a mode)
