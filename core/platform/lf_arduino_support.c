7a901b6ef5a6d3122cffbd64a9e631fd64a9ddc2
Anirudh Rengarajan <44007330+arengarajan99@users.noreply.github.com>
2022-07-26 17:58:30 -0700

    Update core/platform/lf_arduino_support.c for Copyright
    Co-authored-by: Edward A. Lee <eal@eecs.berkeley.edu>

--- core/platform/lf_arduino_support.c
+++ core/platform/lf_arduino_support.c
@@ -1,7 +1,7 @@
 /* Arduino Platform API support for the C target of Lingua Franca. */
 
 /*************
-Copyright (c) 2021, The University of California at Berkeley.
+Copyright (c) 2022, The University of California at Berkeley.
 Redistribution and use in source and binary forms, with or without modification,
 are permitted provided that the following conditions are met:
 1. Redistributions of source code must retain the above copyright notice,
