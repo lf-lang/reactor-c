f057d607806b40482446c9c2c76edd3f71c947e9
Peter Donovan <peterdonovan@berkeley.edu>
2022-07-30 11:59:35 -0700

    Remove more deprecated functions.

--- include/ctarget/tag.h
+++ include/ctarget/tag.h
@@ -14,22 +14,11 @@
  * @return -1, 0, or 1 depending on the relation.
  */
 int lf_tag_compare(tag_t tag1, tag_t tag2);
-DEPRECATED(int compare_tags(tag_t tag1, tag_t tag2));
 
 /**
  * Return the current tag, a logical time, microstep pair.
  */
 tag_t lf_tag();
-
-/**
- * Return the current tag, a logical time, microstep pair.
- */
-DEPRECATED(tag_t get_current_tag(void));
-
-/**
- * Return the current microstep.
- */
-DEPRECATED(microstep_t get_microstep(void));
 
 
 #endif // CTARGET_TAG
