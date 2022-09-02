b70632ce516343216d960c09ad9eb01cc57af02e
Marten Lohstroh <marten@berkeley.edu>
2022-09-02 09:43:46 -0700

    Add file I forgot to check in

---
+++ core/platform/lf_os_single_threaded_support.c
@@ -0,0 +1,15 @@
+int lf_critical_section_enter() {
+    return 0;
+}
+
+int lf_critical_section_exit() {
+    return 0;
+}
+
+int lf_notify_of_event() {
+    return 0;
+}
+
+int lf_init_critical_sections() {
+    return 0;
+}
