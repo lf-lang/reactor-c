ac0a9367f9bb57e49d1d66c6904f6c796ada1faa
Marten Lohstroh <marten@berkeley.edu>
2022-09-03 01:05:42 -0700

    Debugging serialization tests

--- core/federated/net_util.c
+++ core/federated/net_util.c
@@ -617,7 +617,7 @@ bool validate_port(char* port) {
  * Checks if host is valid.
  * @return true if valid, false otherwise.
  */
-bool validate_host(char* host) {
+bool validate_host(const char* host) {
     // regex taken from LFValidator.xtend
     char* ipv4_regex = "((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])";
     char* host_or_FQN_regex = "^([a-z0-9]+(-[a-z0-9]+)*)|(([a-z0-9]+(-[a-z0-9]+)*\\.)+[a-z]{2,})$";
@@ -629,7 +629,7 @@ bool validate_host(char* host) {
  * Checks if user is valid.
  * @return true if valid, false otherwise.
  */
-bool validate_user(char* user) {
+bool validate_user(const char* user) {
     // regex taken from LFValidator.xtend
     char* username_regex = "^[a-z_]([a-z0-9_-]{0,31}|[a-z0-9_-]{0,30}\\$)$";
     return match_regex(user, username_regex);
@@ -639,7 +639,7 @@ bool validate_user(char* user) {
  * Extract one match group from the rti_addr regex .
  * @return true if SUCCESS, else false.
  */
-bool extract_match_group(char* rti_addr, char* dest, regmatch_t group, int max_len, int min_len, char* err_msg) {
+bool extract_match_group(const char* rti_addr, char* dest, regmatch_t group, int max_len, int min_len, char* err_msg) {
     size_t size = group.rm_eo - group.rm_so;
     if (size > max_len || size < min_len) {
         lf_print_error("%s", err_msg);
@@ -654,7 +654,7 @@ bool extract_match_group(char* rti_addr, char* dest, regmatch_t group, int max_l
  * Extract match groups from the rti_addr regex.
  * @return true if success, else false.
  */
-bool extract_match_groups(char* rti_addr, char** rti_addr_strs, bool** rti_addr_flags, regmatch_t* group_array, 
+bool extract_match_groups(const char* rti_addr, char** rti_addr_strs, bool** rti_addr_flags, regmatch_t* group_array, 
                           int* gids, int* max_lens, int* min_lens, char** err_msgs) {
     for (int i = 0; i < 3; i++) {
         if (group_array[gids[i]].rm_so != -1) {
@@ -671,7 +671,7 @@ bool extract_match_groups(char* rti_addr, char** rti_addr_strs, bool** rti_addr_
 /**
  * Extract the host, port and user from rti_addr.  
  */
-void extract_rti_addr_info(char* rti_addr, rti_addr_info_t* rti_addr_info) {
+void extract_rti_addr_info(const char* rti_addr, rti_addr_info_t* rti_addr_info) {
     char* regex_str = "(([a-zA-Z0-9_-]{1,254})@)?([a-zA-Z0-9.]{1,255})(:([0-9]{1,5}))?";
     size_t max_groups = 6;
     // The group indices of each field of interest in the regex.
