0fd8fba1b08ae5f76f14de0366120d21ee8d579c
Erling Rennemo Jellum <erling.r.jellum@ntnu.no>
2022-12-10 19:02:29 -0800

    Fix some typos

--- core/platform/lf_macos_support.c
+++ core/platform/lf_macos_support.c
@@ -34,7 +34,7 @@ THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "tag.h"
 #define LF_MIN_SLEEP_NS USEC(10)
 
-#if defined LF_SINGLE_THREADED || !defined LF_TRACING
+#if defined LF_UNTHREADED && !defined LF_TRACING
     #include "lf_os_single_threaded_support.c"
 #endif
 
