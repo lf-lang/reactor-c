8290d71af0cce879f3a391bbf35898db1b464896
Anirudh Rengarajan <44007330+arengarajan99@users.noreply.github.com>
2023-02-22 15:00:49 -0800

    Apply suggestions from code review

--- include/core/platform/arduino_mbed/MutexWrapper.h
+++ include/core/platform/arduino_mbed/MutexWrapper.h
@@ -1,5 +1,5 @@
 /*************
-Copyright (c) 2022, The University of California at Berkeley.
+Copyright (c) 2023, The University of California at Berkeley.
 
 Redistribution and use in source and binary forms, with or without modification,
 are permitted provided that the following conditions are met:
