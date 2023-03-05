3610879f6f698f327898ffc95ad7660ebcae07cc
lhstrh <marten@berkeley.edu>
2023-03-05 00:47:50 +0000

    Deploying to gh-pages from  @ 573f6741930b3ed8bd57e599bbabd71fc68c6954 🚀

--- search/defines_11.js
+++ search/defines_11.js
@@ -2,13 +2,14 @@ var searchData=
 [
   ['tcp_5ftimeout_5ftime_0',['TCP_TIMEOUT_TIME',['../net__common_8h.html#ab1edbb864391382835b9ad71408c5c53',1,'net_common.h']]],
   ['tracepoint_1',['tracepoint',['../trace_8h.html#aeae0efb071b08e657d36788eee20c1a7',1,'trace.h']]],
-  ['tracepoint_5freaction_5fends_2',['tracepoint_reaction_ends',['../trace_8h.html#a6886c0b07fa8962e9e251bc5442fbae3',1,'trace.h']]],
-  ['tracepoint_5freaction_5fstarts_3',['tracepoint_reaction_starts',['../trace_8h.html#a618fd3947984f4585e5071ec945f4908',1,'trace.h']]],
-  ['tracepoint_5fschedule_4',['tracepoint_schedule',['../trace_8h.html#afce5f2514b7b2001611c45b2f0122cee',1,'trace.h']]],
-  ['tracepoint_5fscheduler_5fadvancing_5ftime_5fends_5',['tracepoint_scheduler_advancing_time_ends',['../trace_8h.html#ac91dba72663dc0f17ce46d105ff3f511',1,'trace.h']]],
-  ['tracepoint_5fscheduler_5fadvancing_5ftime_5fstarts_6',['tracepoint_scheduler_advancing_time_starts',['../trace_8h.html#a2b43a4d257b235afe8066e4a3f1bfe7f',1,'trace.h']]],
-  ['tracepoint_5fuser_5fevent_7',['tracepoint_user_event',['../trace_8h.html#a4eaa42638286bf2cfdfed4b45d4a1036',1,'trace.h']]],
-  ['tracepoint_5fuser_5fvalue_8',['tracepoint_user_value',['../trace_8h.html#adc24126d062442704b43b2ea2f95042c',1,'trace.h']]],
-  ['tracepoint_5fworker_5fwait_5fends_9',['tracepoint_worker_wait_ends',['../trace_8h.html#a9dc20d411fb600c803f04572bde501ff',1,'trace.h']]],
-  ['tracepoint_5fworker_5fwait_5fstarts_10',['tracepoint_worker_wait_starts',['../trace_8h.html#a00635edd06259391fe45fa81f2dd9cab',1,'trace.h']]]
+  ['tracepoint_5freaction_5fdeadline_5fmissed_2',['tracepoint_reaction_deadline_missed',['../trace_8h.html#aa608d6a7b724896211cc9a279bacfbbf',1,'trace.h']]],
+  ['tracepoint_5freaction_5fends_3',['tracepoint_reaction_ends',['../trace_8h.html#a6886c0b07fa8962e9e251bc5442fbae3',1,'trace.h']]],
+  ['tracepoint_5freaction_5fstarts_4',['tracepoint_reaction_starts',['../trace_8h.html#a618fd3947984f4585e5071ec945f4908',1,'trace.h']]],
+  ['tracepoint_5fschedule_5',['tracepoint_schedule',['../trace_8h.html#afce5f2514b7b2001611c45b2f0122cee',1,'trace.h']]],
+  ['tracepoint_5fscheduler_5fadvancing_5ftime_5fends_6',['tracepoint_scheduler_advancing_time_ends',['../trace_8h.html#ac91dba72663dc0f17ce46d105ff3f511',1,'trace.h']]],
+  ['tracepoint_5fscheduler_5fadvancing_5ftime_5fstarts_7',['tracepoint_scheduler_advancing_time_starts',['../trace_8h.html#a2b43a4d257b235afe8066e4a3f1bfe7f',1,'trace.h']]],
+  ['tracepoint_5fuser_5fevent_8',['tracepoint_user_event',['../trace_8h.html#a4eaa42638286bf2cfdfed4b45d4a1036',1,'trace.h']]],
+  ['tracepoint_5fuser_5fvalue_9',['tracepoint_user_value',['../trace_8h.html#adc24126d062442704b43b2ea2f95042c',1,'trace.h']]],
+  ['tracepoint_5fworker_5fwait_5fends_10',['tracepoint_worker_wait_ends',['../trace_8h.html#a9dc20d411fb600c803f04572bde501ff',1,'trace.h']]],
+  ['tracepoint_5fworker_5fwait_5fstarts_11',['tracepoint_worker_wait_starts',['../trace_8h.html#a00635edd06259391fe45fa81f2dd9cab',1,'trace.h']]]
 ];
