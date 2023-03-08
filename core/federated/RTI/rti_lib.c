7995d88330964908094d205658b45f10c7a96028
Byeonggil-Jun <junbg@hanyang.ac.kr>
2023-03-08 09:12:27 +0900

    First commit for fowarding NET message to optimize RTI

--- core/federated/RTI/rti_lib.c
+++ core/federated/RTI/rti_lib.c
@@ -566,7 +566,10 @@ void update_federate_next_event_tag_locked(uint16_t federate_id, tag_t next_even
         next_event_tag = min_in_transit_tag;
     }
 
-    _RTI.federates[federate_id].next_event = next_event_tag;
+    if (lf_tag_compare(_RTI.federates[federate_id].next_event, next_event_tag) != 0) {
+        _RTI.federates[federate_id].next_event = next_event_tag;
+        //FIXME: notify upstream federates
+    }
 
     LF_PRINT_DEBUG(
        "RTI: Updated the recorded next event tag for federate %d to (%ld, %u).",
