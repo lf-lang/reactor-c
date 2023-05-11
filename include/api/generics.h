f1d41011e454a026741beb68bd9a3ec80101c8ee
mkhubaibumer <khubaib@magnition.io>
2023-05-11 19:27:07 +0500

    Rename macros to LF convention

--- include/api/generics.h
+++ include/api/generics.h
@@ -38,41 +38,41 @@ THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 /// If buitin are not available on target toolchain we may not be able to support generics
 #if defined __has_builtin
 /// Auto-Deduce variable type based on assigned value
-#define auto_t __auto_type
+#define var __auto_type
 
 /// Checks if types of both `a` and `b` are same
-#define is_same_type(a, b) __builtin_types_compatible_p(__typeof__(a), __typeof__(b))
+#define lf_is_same_type(a, b) __builtin_types_compatible_p(__typeof__(a), __typeof__(b))
 
 /// Checks if type of `b` is same as the specified `typename`
-#define is_same(typename, b) __builtin_types_compatible_p(typename, __typeof__(b))
+#define lf_is_same(typename, b) __builtin_types_compatible_p(typename, __typeof__(b))
 
 /// Checks if `typename_a` and `typename_b` are same
-#define is_type_equal(typename_a, typename_b) __builtin_types_compatible_p(typename_a, typename_b)
+#define lf_is_type_equal(typename_a, typename_b) __builtin_types_compatible_p(typename_a, typename_b)
 
 /// Checks if the passed variable `p` is array or a pointer
-#define is_pointer_or_array(p)  (__builtin_classify_type(p) == 5)
+#define lf_is_pointer_or_array(p)  (__builtin_classify_type(p) == 5)
 
-#define decay(p)  (&*__builtin_choose_expr(is_pointer_or_array(p), p, NULL))
+#define lf_decay(p)  (&*__builtin_choose_expr(lf_is_pointer_or_array(p), p, NULL))
 
 /// Checks if passed variable `p` is a pointer
-#define is_pointer(p)  is_same_type(p, decay(p))
+#define lf_is_pointer(p)  lf_is_same_type(p, lf_decay(p))
 
 /// Returns the pointer for specified `p`
-#define get_pointer(p) __builtin_choose_expr(is_pointer(p), p, &p)
+#define lf_get_pointer(p) __builtin_choose_expr(lf_is_pointer(p), p, &p)
 
 /// Checks types for both `left` and `right` and returns appropriate value based on `left` type
-#define to_left_type(left, right) __builtin_choose_expr(is_pointer_or_array(left), __builtin_choose_expr(is_pointer_or_array(right), (right), &(right)), __builtin_choose_expr(is_pointer_or_array(right), *(right), (right)))
+#define lf_to_left_type(left, right) __builtin_choose_expr(lf_is_pointer_or_array(left), __builtin_choose_expr(lf_is_pointer_or_array(right), (right), &(right)), __builtin_choose_expr(lf_is_pointer_or_array(right), *(right), (right)))
 
 #else // buitin are not available
 
-#define auto_t
-#define is_same_type(a, b)
-#define is_same(typename, b)
-#define is_pointer_or_array(p)
-#define decay(p)
-#define is_pointer(p)
-#define get_pointer(p)
-#define to_left_type(left, right)
+#define var
+#define lf_is_same_type(a, b)
+#define lf_is_same(typename, b)
+#define lf_is_pointer_or_array(p)
+#define lf_decay(p)
+#define lf_is_pointer(p)
+#define lf_get_pointer(p)
+#define lf_to_left_type(left, right)
 
 #endif // __has_builtin
 
