e27c7471869b4d721d8c7e79dda69ccc4b51c189
Abhinav Gundrala <gundralaa@berkeley.edu>
2023-07-22 15:09:30 -0500

    [docs] modify/resolve support doc comments

--- core/platform/lf_rp2040_support.c
+++ core/platform/lf_rp2040_support.c
@@ -39,47 +39,42 @@ THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <pico/multicore.h>
 #include <pico/sync.h>
 
-// critical section struct binding
-// TODO: maybe be more precise and use nvic interrupt mask
-static critical_section_t _lf_crit_sec;
-// semaphore used to notify if sleep was interupted by irq 
-static semaphore_t _lf_sem_irq_event;
-static uint32_t _lf_num_nested_critical_sections = 0;
-
-/**
- * Initialize the LF clock. Must be called before using other clock-related APIs.
+/* 
+ * critical section struct
+ * disables external irq and core execution
+ * provides mutual exclusion using hardware spin-locks
  */
+static critical_section_t _lf_crit_sec;
+
+/*
+ * binary semaphore for lf event notification 
+ * used by external isr or core threads
+ * used to interact with the lf runtime thread
+ */
+static semaphore_t _lf_sem_irq_event;
+
+// nested critical section counter
+static uint32_t _lf_num_nested_crit_sec = 0;
+
 void _lf_initialize_clock(void) {
-    // init stdlib peripherals
+    // init stdio lib
     stdio_init_all();
     // init sync structs
     critical_section_init(&_lf_crit_sec);
     sem_init(&_lf_sem_irq_event, 0, 1);
 }
 
-/**
- * Fetch the value of an internal (and platform-specific) physical clock and
- * store it in `t`. in nanoseconds
- *
- * Ideally, the underlying platform clock should be monotonic. However, the
- * core lib tries to enforce monotonicity at higher level APIs (see tag.h).
- * TODO: might want to use the RTC
- * @return 0 for success, or -1 for failure
- */
 int _lf_clock_now(instant_t* t) {
+    // time struct
     absolute_time_t now;
     uint64_t ns_from_boot;
+
     now = get_absolute_time();
     ns_from_boot = to_us_since_boot(now) * 1000;
     *t = (instant_t) ns_from_boot;
     return 0; 
 }
 
-/**
- * Pause execution for a given duration.
- * 
- * @return 0 for success, or -1 for failure.
- */
 int lf_sleep(interval_t sleep_duration) {
     if (sleep_duration < 0) {
         return -1;
@@ -88,90 +83,77 @@ int lf_sleep(interval_t sleep_duration) {
     return 0;
 }
 
-/**
- * @brief Sleep until the given wakeup time.
- * 
- * @param wakeup_time The time instant at which to wake up.
- * @return int 0 if sleep completed, or -1 if it was interrupted.
- */
 int _lf_interruptable_sleep_until_locked(environment_t* env, instant_t wakeup_time) {
     int ret_code = 0;
+    // return error
     if (wakeup_time < 0) {
         ret_code = -1;
         return ret_code;
     }
+    // time struct
     absolute_time_t target;
-    // reset semaphore to 0
-    // TODO: leverage the semaphore permit number 
+    
+    // reset event semaphore 
     sem_reset(&_lf_sem_irq_event, 0);
+    // create us boot wakeup time
     target = from_us_since_boot((uint64_t) (wakeup_time / 1000));
+    // allow interrupts
     lf_critical_section_exit(env);
-    // sleep till target or return on processor event
+    // blocked sleep
+    // return on timeout or on processor event
     if(sem_acquire_block_until(&_lf_sem_irq_event, target)) {
         ret_code = -1;
     }
+    // remove interrupts
     lf_critical_section_enter(env);
     return ret_code;
 }
-/*
-* Critical sections are only provided for an unthreaded, single core
-* runtime. In the unthreaded runtime, all interactions with core1 are through
-* physical actions and interupts outside of the runtime.
-*/
-#ifdef LF_UNTHREADED
 
-/**
- * Enter a critical section where logical time and the event queue are guaranteed
- * to not change unless they are changed within the critical section.
- * this can be implemented by disabling interrupts.
- * Users of this function must ensure that lf_init_critical_sections() is
- * called first and that lf_critical_section_exit() is called later.
- * @return 0 on success, platform-specific error number otherwise.
- * TODO: needs to be used sparingly 
+#ifdef LF_UNTHREADED
+/*
+ * The single thread rp2040 platform support treats second core
+ * routines similar to external interrupt routine threads.
+ *
+ * Second core activity is disabled at the same times as
+ * when interrupts are disabled. 
  */
 int lf_disable_interrupts_nested() {
     if (!critical_section_is_initialized(&_lf_crit_sec)) {
         return 1;
-    } 
-    // disables irq and spin-locks core
-    if (_lf_num_nested_critical_sections++ == 0) {
+    }
+    // check crit sec count
+    // enter non-rentrant state
+    // prevent second core access and disable interrupts
+    if (_lf_num_nested_crit_sec == 0) {
+        // block if associated spin lock in use
         critical_section_enter_blocking(&_lf_crit_sec);
     }
+    // add crit sec count
+    _lf_num_nested_crit_sec++;
     return 0;
 }
 
-/**
- * Exit the critical section entered with lf_lock_time().
- * @return 0 on success, platform-specific error number otherwise.
- * TODO: needs to be used sparingly, find a better way for event queue
- * mutual exclusion for embedded platforms. better leverage the nvic 
- */
 int lf_enable_interrupts_nested() {
     if (!critical_section_is_initialized(&_lf_crit_sec) ||
-        _lf_num_nested_critical_sections <= 0) {
+        _lf_num_nested_crit_sec <= 0) {
         return 1;
-    } 
-    // restores system execution state
-    if (--_lf_num_nested_critical_sections == 0) {
+    }
+    // remove from crit sec count
+    _lf_num_nested_crit_sec--;
+    // check crit sec count
+    // release spin-lock
+    if (_lf_num_nested_crit_sec == 0) {
         critical_section_exit(&_lf_crit_sec);
     }
     return 0;
 }
 
-/**
- * Notify any listeners that an event has been created.
- * The caller should call lf_critical_section_enter() before calling this function.
- * @return 0 on success, platform-specific error number otherwise.
- */
 int _lf_unthreaded_notify_of_event() {
-    // un-block threads that acquired this binary semaphore 
+    // notify main sleep loop of event
     sem_release(&_lf_sem_irq_event);
     return 0;
 }
 #endif //LF_UNTHREADED
-
-// For platforms with threading support, the following functions
-// abstract the API so that the LF runtime remains portable.
 
 #ifdef LF_THREADED
 #error "Threading for baremetal RP2040 not supported"
