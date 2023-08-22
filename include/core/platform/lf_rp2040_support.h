f96451aff182e187c725dcc8b075ebd40ff46a77
Abhi Gundrala <gundralaa@berkeley.edu>
2023-08-21 23:24:50 -0700

    add threading typedefs

--- include/core/platform/lf_rp2040_support.h
+++ include/core/platform/lf_rp2040_support.h
@@ -21,7 +21,36 @@
 #define _LF_TIMEOUT 1
 
 #ifdef LF_THREADED
-#error "Threading for baremetal RP2040 not supported" 
-#endif // LF_THREADED
 
+// function ptr typedef
+// @param void ptr
+// @return void ptr
+typedef void *(*lf_function_t) (void *);
+
+// mutex primitive
+// backed by hardware spinlock
+typedef recursive_mutex_t lf_mutex_t;
+
+// condition var primitive
+// FIXME: repetitive implementation of semaphore
+typedef semaphore_t lf_cond_t;
+
+// thread id type
+// use enum due to limited number of workers
+typedef enum {
+  RP2040_CORE_0 = 0,
+  RP2040_CORE_1 = 1,
+} lf_thread_t;
+
+// core1 entry method
+void _rp2040_core1_entry();
+
+// atomics
+int _rp2040_atomic_fetch_add(int *ptr, int value);
+int _rp2040_atomic_add_fetch(int *ptr, int value);
+bool _rp2040_bool_compare_and_swap(bool *ptr, bool value, bool newval);
+int _rp2040_val_compare_and_swap(int *ptr, int value, int newval);
+
+
+#endif // LF_THREADED
 #endif // LF_PICO_SUPPORT_H
