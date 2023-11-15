7dba9f3c7d7d83cf6b21ca4e79b78f4dbb681ade
Byeong-gil Jun <junbg@hanyang.ac.kr>
2023-11-15 10:40:30 +0900

    Add a function for searching the tag and a test for it

--- test/general/utils/pqueue_test.c
+++ test/general/utils/pqueue_test.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <assert.h>
 #include <string.h>
 #include "pqueue_tag.h"
@@ -22,12 +23,42 @@ static void insert_on_queue(pqueue_tag_t* q) {
     assert(!pqueue_tag_insert_tag(q, t2));
     assert(!pqueue_tag_insert_tag(q, t3));
     assert(!pqueue_tag_insert_tag(q, t4));
-
-    assert(pqueue_tag_insert_tag(q, t4));
-    assert(pqueue_tag_insert_tag(q, t1));
     printf("======== Contents of the queue:\n");
     pqueue_print((pqueue_t*)q, NULL);
     assert(pqueue_tag_size(q) == 4);
+}
+
+static void find_from_queue(pqueue_tag_t* q) {
+    tag_t t1 = {.time = USEC(3), .microstep = 0};
+    tag_t t2 = {.time = USEC(2), .microstep = 1};
+    tag_t t3 = {.time = USEC(2), .microstep = 0};
+    tag_t t4 = {.time = USEC(1), .microstep = 2};
+    tag_t t5 = {.time = USEC(0), .microstep = 0};
+    tag_t t6 = {.time = USEC(3), .microstep = 2};
+    pqueue_tag_element_t* d_t1 = (pqueue_tag_element_t*) malloc(sizeof(pqueue_tag_element_t));
+    d_t1->is_dynamic = 1;
+    d_t1->tag = t1;
+    pqueue_tag_element_t* d_t2 = (pqueue_tag_element_t*) malloc(sizeof(pqueue_tag_element_t));
+    d_t2->is_dynamic = 1;
+    d_t2->tag = t2;
+    pqueue_tag_element_t* d_t3 = (pqueue_tag_element_t*) malloc(sizeof(pqueue_tag_element_t));
+    d_t3->is_dynamic = 1;
+    d_t3->tag = t3;
+    pqueue_tag_element_t* d_t4 = (pqueue_tag_element_t*) malloc(sizeof(pqueue_tag_element_t));
+    d_t4->is_dynamic = 1;
+    d_t4->tag = t4;
+    pqueue_tag_element_t* d_t5 = (pqueue_tag_element_t*) malloc(sizeof(pqueue_tag_element_t));
+    d_t5->is_dynamic = 1;
+    d_t5->tag = t5;
+    pqueue_tag_element_t* d_t6 = (pqueue_tag_element_t*) malloc(sizeof(pqueue_tag_element_t));
+    d_t6->is_dynamic = 1;
+    d_t6->tag = t6;
+    assert(pqueue_tag_find_same_tag(q, d_t1, 1) != NULL);
+    assert(pqueue_tag_find_same_tag(q, d_t2, 1) != NULL);
+    assert(pqueue_tag_find_same_tag(q, d_t3, 1) != NULL);
+    assert(pqueue_tag_find_same_tag(q, d_t4, 1) != NULL);
+    assert(pqueue_tag_find_same_tag(q, d_t5, 1) == NULL);
+    assert(pqueue_tag_find_same_tag(q, d_t6, 1) == NULL);
 }
 
 static void pop_from_queue(pqueue_tag_t* q) {
@@ -64,6 +95,7 @@ int main(int argc, char *argv[]) {
     pqueue_tag_t* q = pqueue_tag_init(2);
 
     insert_on_queue(q);
+    find_from_queue(q);
     pop_from_queue(q);
     pop_empty(q);
 
