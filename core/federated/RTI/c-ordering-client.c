16c5923790cab79a5e89ea3ffc7eb4925d499d1c
Peter Donovan <peterdonovan@berkeley.edu>
2023-11-25 16:15:08 -0800

    Use nops when the dylib isn't provided.
    This is one way to disable it. Probably it would be safer to be more
    explicit about disabling it, but I'm in a hurry :).

--- core/federated/RTI/c-ordering-client.c
+++ core/federated/RTI/c-ordering-client.c
@@ -6,11 +6,25 @@ const char* LIBRARY_PATH_ENV_VAR = "C_ORDERING_CLIENT_LIBRARY_PATH";
 
 #include "c-ordering-client.h"
 
+ClientAndJoinHandle start_client_nop(int federate_id) {
+  return (ClientAndJoinHandle) { .client = NULL, .join_handle = NULL };
+}
+void drop_join_handle_nop(void* join_handle) {}
+void tracepoint_maybe_wait_nop(void* client, char* hook_id, int federate_id, int sequence_number) {}
+void tracepoint_maybe_notify_nop(void* client, char* hook_id, int federate_id, int sequence_number) {}
+void tracepoint_maybe_do_nop(void* client, char* hook_id, int federate_id, int sequence_number) {}
+
 OrderingClientApi* load_ordering_client_api() {
   char* library_path = getenv(LIBRARY_PATH_ENV_VAR);
   if (!library_path) {
     fprintf(stderr, "environment variable %s not set\n", LIBRARY_PATH_ENV_VAR);
-    exit(1);
+    OrderingClientApi* api = malloc(sizeof(OrderingClientApi));
+    api->start_client = start_client_nop;
+    api->drop_join_handle = drop_join_handle_nop;
+    api->tracepoint_maybe_wait = tracepoint_maybe_wait_nop;
+    api->tracepoint_maybe_notify = tracepoint_maybe_notify_nop;
+    api->tracepoint_maybe_do = tracepoint_maybe_do_nop;
+    return api;
   }
   void* handle = dlopen(library_path, RTLD_LAZY);
   if (!handle) {
