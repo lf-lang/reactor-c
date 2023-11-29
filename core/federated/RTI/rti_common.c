8b0d3411f71debe4b824473b59eb8172a7a2f74f
Edward A. Lee <eal@berkeley.edu>
2023-11-28 17:54:14 -0800

    Fixed debug message

--- core/federated/RTI/rti_common.c
+++ core/federated/RTI/rti_common.c
@@ -318,7 +318,7 @@ void update_min_delays_upstream(scheduling_node_t* node) {
         // Put the results onto the node's struct.
         node->num_min_delays = count;
         node->min_delays = (minimum_delay_t*)malloc(count * sizeof(minimum_delay_t));
-        LF_PRINT_DEBUG("++++ Node %hu(is in ZDC: %d\n", node->id, node->flags & IS_IN_ZERO_DELAY_CYCLE);
+        LF_PRINT_DEBUG("++++ Node %hu is in ZDC: %d", node->id, is_in_zero_delay_cycle(node));
         int k = 0;
         for (int i = 0; i < rti_common->number_of_scheduling_nodes; i++) {
             if (lf_tag_compare(path_delays[i], FOREVER_TAG) < 0) {
