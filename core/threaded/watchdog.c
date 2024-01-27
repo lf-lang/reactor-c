81d8cbf0d4c223a43083fc4011b07b9d94e69b0a
erlingrj <erling.jellum@gmail.com>
2024-01-27 18:57:18 +0100

    Watchdog implementation with cond var

--- core/threaded/watchdog.c
+++ core/threaded/watchdog.c
@@ -21,8 +21,10 @@ extern watchdog_t* _lf_watchdogs;
  */
 void _lf_initialize_watchdog_mutexes() {
     for (int i = 0; i < _lf_watchdog_count; i++) {
-        self_base_t* current_base = _lf_watchdogs[i].base;
-        if (current_base->reactor_mutex != NULL) {
+        watchdog_t * watchdog = &_lf_watchdogs[i];
+        lf_mutex_init(&watchdog->lock);
+        lf_cond_init(&watchdog->cond, &watchdog->lock);
+        if (watchdog->base.reactor_mutex != NULL) {
             lf_mutex_init((lf_mutex_t*)(current_base->reactor_mutex));
         }
     }
@@ -36,12 +38,18 @@ void _lf_initialize_watchdog_mutexes() {
  * sleeping, so the watchdog never expires and the handler function is never
  * invoked.
  * This function acquires the reaction mutex and releases it while sleeping.
+ * The thread sleeps by waiting on a condition variable. This allows for other
+ * threads terminating the watchdog safely. 
  * 
  * @param arg A pointer to the watchdog struct
  * @return NULL
  */
 void* _lf_run_watchdog(void* arg) {
     watchdog_t* watchdog = (watchdog_t*)arg;
+
+    // Acquire the global watchdog mutex. This is just used for coordinating
+    // the graceful shutdown of the watchdog
+    lf_mutex_lock(&watchdog->mutex);
 
     self_base_t* base = watchdog->base;
     assert(base->reactor_mutex != NULL);
@@ -50,18 +58,28 @@ void* _lf_run_watchdog(void* arg) {
     while (physical_time < watchdog->expiration) {
         interval_t T = watchdog->expiration - physical_time;
         lf_mutex_unlock((lf_mutex_t*)base->reactor_mutex);
-        lf_sleep(T);
+
+        // Wait for expiration, or a signal to terminate
+        if(lf_cond_timedwait(&watchdog->cond, watchdog->expiration) != LF_TIMEOUT) {
+            // We got a signal to terminate. Release the mutex associated with
+            // the watchdog and terminate the thread.
+            lf_mutex_unlock(&watchdog->mutex);
+            watchdog->thread_active = false;
+            return NULL;
+        }
+        // We had a timeout. Lock the reactor-mutex and verify that we havent
+        // received a new budget.
         lf_mutex_lock((lf_mutex_t*)(base->reactor_mutex));
         physical_time = lf_time_physical();
     }
 
-    if (watchdog->expiration != NEVER) {
-        watchdog_function_t watchdog_func = watchdog->watchdog_function;
-        (*watchdog_func)(base);
-    }
+    // At this point we have a timeout.
+    watchdog_function_t watchdog_func = watchdog->watchdog_function;
+    (*watchdog_func)(base);
+  
+    // Here the thread terminates. 
     watchdog->thread_active = false;
-
-    lf_mutex_unlock((lf_mutex_t*)(base->reactor_mutex));
+    lf_mutex_unlock(watchdog->mutex)
     return NULL;
 }
 
@@ -72,6 +90,7 @@ void lf_watchdog_start(watchdog_t* watchdog, interval_t additional_timeout) {
 
     watchdog->expiration = base->environment->current_tag.time + watchdog->min_expiration + additional_timeout;
 
+    // If we dont have a running watchdog thread.
     if (!watchdog->thread_active) {
         lf_thread_create(&(watchdog->thread_id), _lf_run_watchdog, watchdog);
         watchdog->thread_active = true;
@@ -79,5 +98,12 @@ void lf_watchdog_start(watchdog_t* watchdog, interval_t additional_timeout) {
 }
 
 void lf_watchdog_stop(watchdog_t* watchdog) {
-    watchdog->expiration = NEVER;
+    if (!watchdog->thread_active)
+        return;
+    
+    lf_mutex_lock(watchdog->lock);
+    lf_cond_signal(watchdog->cond);
+    lf_mutex_unlock(watchdog->lock);
+    void *thread_return;
+    lf_thread_join(watchdog->thread_id, &thread_return);
 }
