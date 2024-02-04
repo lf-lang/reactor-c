1a1ec8f5ba50d442be4271a1e4011f8b4420cb5a
erlingrj <erling.jellum@gmail.com>
2024-02-04 15:19:35 +0100

    Typo

--- include/core/platform/lf_atomic.h
+++ include/core/platform/lf_atomic.h
@@ -10,7 +10,7 @@ int32_t lf_atomic_add_fetch32(int32_t * ptr, int32_t val);
 int64_t lf_atomic_add_fetch64(int64_t * ptr, int64_t val);
 
 
-bool lf_bool_compare_and_swap(bool ptr, bool value, bool newval);
+bool lf_bool_compare_and_swap(bool* ptr, bool value, bool newval);
 
 int32_t lf_val32_compare_and_swap(int32_t *ptr, int32_t val, int32_t newval);
 int64_t lf_val64_compare_and_swap(int64_t *ptr, int64_t val, int64_t newval);
