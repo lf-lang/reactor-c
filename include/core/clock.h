e36f10f6c690d6a1de001b676b290b81377401e4
erling <erling.jellum@gmail.com>
2024-02-11 11:08:40 +0100

    Apply suggestions from code review
    Co-authored-by: Edward A. Lee <eal@eecs.berkeley.edu>

--- include/core/clock.h
+++ include/core/clock.h
@@ -4,9 +4,10 @@
  * @copyright (c) 2024
  * License: <a href="https://github.com/lf-lang/reactor-c/blob/main/LICENSE.md">BSD 2-clause</a>
  * @brief A higher level API to the clock utilities provided by the platform API.
- * It builds ontop of the clocking API of the different platforms and ensures:
+ *
+ * This builds on top of the clocking API of the different platforms and ensures:
  * 1. Monotonicity
- * 2. That clock synchronization offsets are applied and removed.
+ * 2. That clock synchronization offsets are applied and removed when necessary.
  */
 
 #ifndef CLOCK_H
@@ -15,8 +16,8 @@
 #include "platform.h"
 
 /**
- * This will block the calling thread until wakeup_time is reached or it is
- * interrupted by an asynchronous scheduling. Used by the single-threaded
+ * Block the calling thread until wakeup_time is reached or the thread is
+ * interrupted by an asynchronous scheduling. This is used by the single-threaded
  * runtime. Before calling the appropriate function in the platform API, the
  * wakeup_time will be translated into the correct timescale by removing any
  * clock synchronization offset.
@@ -27,16 +28,17 @@ int lf_clock_interruptable_sleep_until_locked(environment_t *env, instant_t wake
 
 
 /**
- * Retrives the current physical time from the platform API. It adjusts for
- * clock synchronization and guaranteed monotonicity.
- * @param now 
- * @return 0 on success. 
+ * Retrieve the current physical time from the platform API. This adds any clock synchronization offset
+ * and guarantees monotonicity. Specifically, each returned value will be at least one nanosecond larger
+ * than any previously returned time.
+ * @param now A pointer to the location in which to store the result.
+ * @return 0 on success, -1 on failure to read the platform clock.
  */
 int lf_clock_gettime(instant_t *now);
 
 #if !defined(LF_SINGLE_THREADED)
 /**
- * This will block the calling thread on the condition variable until it is
+ * Block the calling thread on the condition variable until it is
  * signaled or until wakeup_time is reached. Before calling the appropriate
  * function in the platform API, the wakeup_time will be translated into the
  * correct timescale by removing any clock synchronization offset.
