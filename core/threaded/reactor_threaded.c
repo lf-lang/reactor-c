485bb94720c8f1825bab9e87970dc27439ba42ea
Chadlia Jerad <chadlia.jerad@gmail.com>
2024-02-20 11:11:55 +0100

    Adjust doc

--- core/threaded/reactor_threaded.c
+++ core/threaded/reactor_threaded.c
@@ -613,7 +613,7 @@ void _lf_initialize_start_tag(environment_t* env) {
   LF_PRINT_DEBUG("Physical time is ahead of current time by " PRINTF_TIME ". This should be close to the STA offset.",
                  lf_time_physical() - start_time);
 
-  // Restore the current tag to match the start time.
+  // Restore the current tag to match the effective start time.
   env->current_tag = (tag_t){.time = effective_start_tag.time, .microstep = effective_start_tag.microstep};
 
   // If the stop_tag is (0,0), also insert the shutdown
