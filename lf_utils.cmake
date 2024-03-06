9f5cc39d8694eafeda397c824963078c6867d5f6
erlingrj <erling.jellum@gmail.com>
2024-03-06 15:35:32 +0100

    CMake cleanup: - Move linking with threading library to low-level platform with PRIVATE -  Remove the wierd REACTORC_COMPILE_DEFS variable - Dont add source files when they are not to be compiled - Add custom function for enabling warnings and -Werror

---
+++ lf_utils.cmake
@@ -0,0 +1,7 @@
+function(lf_enable_compiler_warnings target)
+    if(MSVC)
+        target_compile_options(${target} PRIVATE /W4 /WX)
+    else()
+        target_compile_options(${target} PRIVATE -Wall -Wextra -pedantic -Werror)
+    endif()
+endfunction()
