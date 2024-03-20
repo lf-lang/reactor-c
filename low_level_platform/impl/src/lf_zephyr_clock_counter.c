15d82526c3ee86910bc18881f090b77a8c0f21bd
erlingrj <erling.jellum@gmail.com>
2024-03-20 15:54:11 +0100

    Make the zephyr kernel clock the default clock on Zephyr

--- low_level_platform/impl/src/lf_zephyr_clock_counter.c
+++ low_level_platform/impl/src/lf_zephyr_clock_counter.c
@@ -24,7 +24,6 @@ INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT
 STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
 THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ***************/
-
 /**
  * @brief This implements the timing-related platform API ontop of the Zephyr
  * Counter API. The Counter API is a generic interface to a timer peripheral. It
