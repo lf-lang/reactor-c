4ad2cdf3fbd3297736fee2ff52bb63197df3c4d7
Shaokai Lin <shaokai_lin@126.com>
2024-03-22 11:05:37 -0700

    Back to 32k

--- include/core/trace.h
+++ include/core/trace.h
@@ -253,7 +253,7 @@ static const char *trace_event_names[] = {
 
 // FIXME: Target property should specify the capacity of the trace buffer.
 // #define TRACE_BUFFER_CAPACITY 2048
-#define TRACE_BUFFER_CAPACITY 16384
+#define TRACE_BUFFER_CAPACITY 32768
 
 /** Size of the table of trace objects. */
 #define TRACE_OBJECT_TABLE_SIZE 1024
