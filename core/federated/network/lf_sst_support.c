b83f35b8040f5a8b8121746f06a3cff769e57ed7
Dongha Kim <dongha@asu.edu>
2024-03-23 14:02:59 -0700

    Add lf_sst_support.c .h bases

--- core/federated/network/lf_sst_support.c
+++ core/federated/network/lf_sst_support.c
@@ -21,47 +21,47 @@
 
 typedef enum { TCP, UDP } socket_type_t;
 
-static sst_socket_priv_t* get_priv(netdrv_t* drv) {
+static socket_priv_t* get_priv(netdrv_t* drv) {
   if (!drv) {
-    // lf_print_error_and_exit("Falied get sst_socket_priv_t.");
+    // lf_print_error_and_exit("Falied get socket_priv_t.");
     // FIXME(Byeonggil): I commented out this because it shouldn't report error to send -1 port address.
     return NULL;
   }
-  return (sst_socket_priv_t*)(drv + 1);
+  return (socket_priv_t*)(drv + 1);
 }
 
 char* get_host_name(netdrv_t* drv) {
-  sst_socket_priv_t* priv = get_priv(drv);
+  socket_priv_t* priv = get_priv(drv);
   return priv->server_hostname;
 }
 int32_t get_my_port(netdrv_t* drv) {
-  sst_socket_priv_t* priv = get_priv(drv);
+  socket_priv_t* priv = get_priv(drv);
   return priv->port;
 }
 int32_t get_port(netdrv_t* drv) {
-  sst_socket_priv_t* priv = get_priv(drv);
+  socket_priv_t* priv = get_priv(drv);
   return (priv == NULL) ? -1 : priv->server_port;
 }
 struct in_addr* get_ip_addr(netdrv_t* drv) {
-  sst_socket_priv_t* priv = get_priv(drv);
+  socket_priv_t* priv = get_priv(drv);
   return &priv->server_ip_addr;
 }
 void set_host_name(netdrv_t* drv, const char* hostname) {
-  sst_socket_priv_t* priv = get_priv(drv);
+  socket_priv_t* priv = get_priv(drv);
   memcpy(priv->server_hostname, hostname, INET_ADDRSTRLEN);
 }
 void set_port(netdrv_t* drv, int port) {
-  sst_socket_priv_t* priv = get_priv(drv);
+  socket_priv_t* priv = get_priv(drv);
   priv->server_port = port;
 }
 void set_ip_addr(netdrv_t* drv, struct in_addr ip_addr) {
-  sst_socket_priv_t* priv = get_priv(drv);
+  socket_priv_t* priv = get_priv(drv);
   priv->server_ip_addr = ip_addr;
 }
 
 void set_clock_netdrv(netdrv_t* clock_drv, netdrv_t* rti_drv, uint16_t port_num) {
-  sst_socket_priv_t* priv_clock = get_priv(clock_drv);
-  sst_socket_priv_t* priv_rti = get_priv(rti_drv);
+  socket_priv_t* priv_clock = get_priv(clock_drv);
+  socket_priv_t* priv_rti = get_priv(rti_drv);
   priv_clock->UDP_addr.sin_family = AF_INET;
   priv_clock->UDP_addr.sin_port = htons(port_num);
   priv_clock->UDP_addr.sin_addr = priv_rti->server_ip_addr;
@@ -72,7 +72,7 @@ static int socket_open(netdrv_t* drv) {
   if (!drv) {
     return -1;
   }
-  sst_socket_priv_t* priv = get_priv(drv);
+  socket_priv_t* priv = get_priv(drv);
   priv->socket_descriptor = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
   priv->proto = TCP;
   if (priv->socket_descriptor < 0) {
@@ -102,7 +102,7 @@ static int socket_open(netdrv_t* drv) {
 }
 
 void netdrv_free(netdrv_t* drv) {
-  sst_socket_priv_t* priv = get_priv(drv);
+  socket_priv_t* priv = get_priv(drv);
   // free(priv); // Already freed on socket close()
   free(drv);
 }
@@ -111,7 +111,7 @@ static void socket_close(netdrv_t* drv) {
   if (!drv) {
     return;
   }
-  sst_socket_priv_t* priv = get_priv(drv);
+  socket_priv_t* priv = get_priv(drv);
   if (priv->socket_descriptor > 0) {
     shutdown(priv->socket_descriptor, SHUT_RDWR);
     close(priv->socket_descriptor);
@@ -121,13 +121,13 @@ static void socket_close(netdrv_t* drv) {
 
 netdrv_t* netdrv_init() {
   // TODO: Should it be malloc? To support different network stacks operate simulatneously?
-  netdrv_t* drv = malloc(sizeof(*drv) + sizeof(sst_socket_priv_t)); // Don't need to call malloc() twice.
+  netdrv_t* drv = malloc(sizeof(*drv) + sizeof(socket_priv_t)); // Don't need to call malloc() twice.
   if (!drv) {                                                   // check if malloc worked.
     lf_print_error_and_exit("Falied to malloc netdrv_t.");
   }
   memset(drv, 0, sizeof(netdrv_t));
 
-  sst_socket_priv_t* priv = get_priv(drv);
+  socket_priv_t* priv = get_priv(drv);
   priv->port = 0;
   priv->socket_descriptor = 0;
   priv->user_specified_port = 0;
@@ -186,7 +186,7 @@ static int net_create_real_time_tcp_socket_errexit() {
 
 // TODO: DONGHA: Need to fix port.
 int create_federate_server(netdrv_t* drv, uint16_t port, int specified_port) {
-  sst_socket_priv_t* priv = get_priv(drv);
+  socket_priv_t* priv = get_priv(drv);
   // Server file descriptor.
   struct sockaddr_in server_fd;
   // Zero out the server address structure.
@@ -243,7 +243,7 @@ int create_federate_server(netdrv_t* drv, uint16_t port, int specified_port) {
  */
 // TODO: Fix comments.
 int create_rti_server(netdrv_t* drv, netdrv_type_t netdrv_type) {
-  sst_socket_priv_t* priv = get_priv(drv);
+  socket_priv_t* priv = get_priv(drv);
 
   // Timeout time for the communications of the server
   struct timeval timeout_time = {.tv_sec = TCP_TIMEOUT_TIME / BILLION, .tv_usec = (TCP_TIMEOUT_TIME % BILLION) / 1000};
@@ -367,8 +367,8 @@ int create_rti_server(netdrv_t* drv, netdrv_type_t netdrv_type) {
 }
 
 // void close_netdrvs(netdrv_t *rti_netdrv, netdrv_t *clock_netdrv) {
-//     sst_socket_priv_t *rti_priv = get_priv(rti_netdrv);
-//     sst_socket_priv_t *clock_priv = get_priv(clock_netdrv);
+//     socket_priv_t *rti_priv = get_priv(rti_netdrv);
+//     socket_priv_t *clock_priv = get_priv(clock_netdrv);
 //     // Shutdown and close the socket that is listening for incoming connections
 //     // so that the accept() call in respond_to_erroneous_connections returns.
 //     // That thread should then check rti->all_federates_exited and it should exit.
@@ -392,8 +392,8 @@ int create_rti_server(netdrv_t* drv, netdrv_type_t netdrv_type) {
 
 netdrv_t* netdrv_accept(netdrv_t* my_netdrv) {
   netdrv_t* client_netdrv = netdrv_init();
-  sst_socket_priv_t* my_priv = get_priv(my_netdrv);
-  sst_socket_priv_t* client_priv = get_priv(client_netdrv);
+  socket_priv_t* my_priv = get_priv(my_netdrv);
+  socket_priv_t* client_priv = get_priv(client_netdrv);
   struct sockaddr client_fd;
   uint32_t client_length = sizeof(client_fd);
   client_priv->socket_descriptor = accept(my_priv->socket_descriptor, &client_fd, &client_length);
@@ -405,8 +405,8 @@ netdrv_t* netdrv_accept(netdrv_t* my_netdrv) {
 
 netdrv_t* accept_connection(netdrv_t* rti_netdrv) {
   netdrv_t* fed_netdrv = netdrv_init();
-  sst_socket_priv_t* rti_priv = get_priv(rti_netdrv);
-  sst_socket_priv_t* fed_priv = get_priv(fed_netdrv);
+  socket_priv_t* rti_priv = get_priv(rti_netdrv);
+  socket_priv_t* fed_priv = get_priv(fed_netdrv);
   // Wait for an incoming connection request.
   struct sockaddr client_fd;
   uint32_t client_length = sizeof(client_fd);
@@ -435,7 +435,7 @@ netdrv_t* accept_connection(netdrv_t* rti_netdrv) {
 }
 
 int netdrv_connect(netdrv_t* drv) {
-  sst_socket_priv_t* priv = get_priv(drv);
+  socket_priv_t* priv = get_priv(drv);
 
   struct addrinfo hints;
   struct addrinfo* result;
@@ -465,7 +465,7 @@ int netdrv_connect(netdrv_t* drv) {
 }
 
 ssize_t peek_from_netdrv(netdrv_t* drv, unsigned char* result) {
-  sst_socket_priv_t* priv = get_priv(drv);
+  socket_priv_t* priv = get_priv(drv);
   ssize_t bytes_read = recv(priv->socket_descriptor, result, 1, MSG_DONTWAIT | MSG_PEEK);
   if (bytes_read < 0 && (errno == EAGAIN || errno == EWOULDBLOCK))
     return 0;
@@ -475,14 +475,14 @@ ssize_t peek_from_netdrv(netdrv_t* drv, unsigned char* result) {
 
 // Temporary function.
 int write_to_netdrv_UDP(netdrv_t* drv, netdrv_t* target, size_t num_bytes, unsigned char* buffer, int flags) {
-  sst_socket_priv_t* priv_drv = get_priv(drv);
-  sst_socket_priv_t* priv_target = get_priv(target);
+  socket_priv_t* priv_drv = get_priv(drv);
+  socket_priv_t* priv_target = get_priv(target);
   return sendto(priv_drv->socket_descriptor, buffer, num_bytes, flags, (struct sockaddr*)&priv_target->UDP_addr,
                 sizeof(priv_target->UDP_addr));
 }
 
 int write_to_netdrv(netdrv_t* drv, size_t num_bytes, unsigned char* buffer) {
-  sst_socket_priv_t* priv = get_priv(drv);
+  socket_priv_t* priv = get_priv(drv);
   if (priv->socket_descriptor < 0) {
     // Socket is not open.
     errno = EBADF;
@@ -702,14 +702,14 @@ static void handle_header_read(netdrv_t* netdrv, unsigned char* buffer, size_t* 
 }
 
 static ssize_t read_UDP(netdrv_t* netdrv, unsigned char* buffer, size_t buffer_length) {
-  sst_socket_priv_t* priv = get_priv(netdrv);
+  socket_priv_t* priv = get_priv(netdrv);
   return read(priv->socket_descriptor, buffer, buffer_length);
 }
 
 // TODO: DONGHA: ADD buffer_length checking.
 //  Returns the total bytes read.
 ssize_t read_from_netdrv(netdrv_t* netdrv, unsigned char* buffer, size_t buffer_length) {
-  sst_socket_priv_t* priv = get_priv(netdrv);
+  socket_priv_t* priv = get_priv(netdrv);
   if (priv->proto == TCP) {
     size_t bytes_to_read;        // The bytes to read in future.
     ssize_t bytes_read = 0;      // The bytes that was read by a single read() function.
---
+++ include/core/federated/network/lf_sst_support.h
@@ -0,0 +1,219 @@
+#ifndef LF_SOCKET_SUPPORT_H
+#define LF_SOCKET_SUPPORT_H
+
+#include <netinet/in.h>  // IPPROTO_TCP, IPPROTO_UDP
+#include <netinet/tcp.h> // TCP_NODELAY
+
+#include "net_util.h"
+#include "lf_socket_support.h"
+#include "c_api.h"
+
+/**
+ * The timeout time in ns for TCP operations.
+ * Default value is 10 secs.
+ */
+#define TCP_TIMEOUT_TIME SEC(10)
+
+/**
+ * The timeout time in ns for UDP operations.
+ * Default value is 1 sec.
+ */
+#define UDP_TIMEOUT_TIME SEC(1)
+
+/**
+ * Maximum number of port addresses that a federate will try to connect to the RTI on.
+ * If you are using automatic ports begining at DEFAULT_PORT, this puts an upper bound
+ * on the number of RTIs that can be running on the same host.
+ */
+#define MAX_NUM_PORT_ADDRESSES 16
+
+/**
+ * Time to wait before re-attempting to bind to a port.
+ * When a process closes, the network stack typically waits between 30 and 120
+ * seconds before releasing the port.  This is to allow for delayed packets so
+ * that a new process does not receive packets from a previous process.
+ * Here, we limit the retries to 60 seconds.
+ */
+#define PORT_BIND_RETRY_INTERVAL SEC(1)
+
+/**
+ * Number of attempts to bind to a port before giving up.
+ */
+#define PORT_BIND_RETRY_LIMIT 60
+
+/**
+ * Default port number for the RTI.
+ * Unless a specific port has been specified by the LF program in the "at"
+ * for the RTI or on the command line, when the RTI starts up, it will attempt
+ * to open a socket server on this port.
+ */
+#define DEFAULT_PORT 15045u
+
+#define DELAY_BETWEEN_SOCKET_RETRIES MSEC(100)
+
+typedef struct sst_socket_priv_t {
+  int port; // my port number
+  int socket_descriptor;
+  int proto;
+  uint16_t user_specified_port;
+
+  // The connected other side's info.
+  char server_hostname[INET_ADDRSTRLEN]; // Human-readable IP address and
+  int32_t server_port;                   // port number of the socket server of the federate
+                                         // if it has any incoming direct connections from other federates.
+                                         // The port number will be -1 if there is no server or if the
+                                         // RTI has not been informed of the port number.
+  struct in_addr server_ip_addr;         // Information about the IP address of the socket
+                                         // server of the federate.
+
+  struct sockaddr_in UDP_addr; // The UDP address for the federate.
+
+} sst_socket_priv_t;
+
+char* get_host_name(netdrv_t* drv);
+int32_t get_my_port(netdrv_t* drv);
+int32_t get_port(netdrv_t* drv);
+struct in_addr* get_ip_addr(netdrv_t* drv);
+
+void set_host_name(netdrv_t* drv, const char* hostname);
+void set_port(netdrv_t* drv, int port);
+void set_ip_addr(netdrv_t* drv, struct in_addr ip_addr);
+void set_clock_netdrv(netdrv_t* clock_drv, netdrv_t* rti_drv, uint16_t port_num);
+
+netdrv_t* netdrv_init();
+
+void netdrv_free(netdrv_t* drv);
+
+int create_federate_server(netdrv_t* drv, uint16_t port, int specified_port);
+
+/**
+ * Create a server and enable listening for socket connections.
+ * If the specified port if it is non-zero, it will attempt to acquire that port.
+ * If it fails, it will repeatedly attempt up to PORT_BIND_RETRY_LIMIT times with
+ * a delay of PORT_BIND_RETRY_INTERVAL in between. If the specified port is
+ * zero, then it will attempt to acquire DEFAULT_PORT first. If this fails, then it
+ * will repeatedly attempt up to PORT_BIND_RETRY_LIMIT times, incrementing the port
+ * number between attempts, with no delay between attempts.  Once it has incremented
+ * the port number MAX_NUM_PORT_ADDRESSES times, it will cycle around and begin again
+ * with DEFAULT_PORT.
+ *
+ * @param port The port number to use or 0 to start trying at DEFAULT_PORT.
+ * @param socket_type The type of the socket for the server (TCP or UDP).
+ * @return The socket descriptor on which to accept connections.
+ */
+// TODO: Update descriptions.
+int create_rti_server(netdrv_t* drv, netdrv_type_t netdrv_type);
+
+// int create_real_time_tcp_socket_errexit();
+// void close_netdrvs(netdrv_t *rti_netdrv, netdrv_t *clock_netdrv);
+
+netdrv_t* netdrv_accept(netdrv_t* my_netdrv);
+
+netdrv_t* accept_connection(netdrv_t* rti_netdrv);
+
+int netdrv_connect(netdrv_t* drv);
+
+/**
+ * Without blocking, peek at the specified socket and, if there is
+ * anything on the queue, put its first byte at the specified address and return 1.
+ * If there is nothing on the queue, return 0, and if an error occurs,
+ * return -1.
+ * @param socket The socket ID.
+ * @param result Pointer to where to put the first byte available on the socket.
+ */
+ssize_t peek_from_netdrv(netdrv_t* drv, unsigned char* result);
+
+int write_to_netdrv_UDP(netdrv_t* drv, netdrv_t* target, size_t num_bytes, unsigned char* buffer, int flags);
+
+/**
+ * Write the specified number of bytes to the specified socket from the
+ * specified buffer. If an error occurs, return -1 and set errno to indicate
+ * the cause of the error. If the write succeeds, return 0.
+ * This function repeats the attempt until the specified number of bytes
+ * have been written or an error occurs. Specifically, errors EAGAIN,
+ * EWOULDBLOCK, and EINTR are not considered errors and instead trigger
+ * another attempt. A delay between attempts is given by
+ * DELAY_BETWEEN_SOCKET_RETRIES.
+ * @param socket The socket ID.
+ * @param num_bytes The number of bytes to write.
+ * @param buffer The buffer from which to get the bytes.
+ * @return 0 for success, -1 for failure.
+ */
+int write_to_netdrv(netdrv_t* drv, size_t num_bytes, unsigned char* buffer);
+
+/**
+ * Write the specified number of bytes to the specified socket using write_to_socket
+ * and close the socket if an error occurs. If an error occurs, this will change the
+ * socket ID pointed to by the first argument to -1 and will return -1.
+ * @param socket Pointer to the socket ID.
+ * @param num_bytes The number of bytes to write.
+ * @param buffer The buffer from which to get the bytes.
+ * @return 0 for success, -1 for failure.
+ */
+int write_to_netdrv_close_on_error(netdrv_t* drv, size_t num_bytes, unsigned char* buffer);
+
+/**
+ * Write the specified number of bytes to the specified socket using
+ * write_to_socket_close_on_error and exit with an error code if an error occurs.
+ * If the mutex argument is non-NULL, release the mutex before exiting.  If the
+ * format argument is non-null, then use it an any additional arguments to form
+ * the error message using printf conventions. Otherwise, print a generic error
+ * message.
+ * @param socket Pointer to the socket ID.
+ * @param num_bytes The number of bytes to write.
+ * @param buffer The buffer from which to get the bytes.
+ * @param mutex If non-NULL, the mutex to unlock before exiting.
+ * @param format A format string for error messages, followed by any number of
+ *  fields that will be used to fill the format string as in printf, or NULL
+ *  to print a generic error message.
+ */
+void write_to_netdrv_fail_on_error(netdrv_t* drv, size_t num_bytes, unsigned char* buffer, lf_mutex_t* mutex,
+                                   char* format, ...);
+
+/**
+ * Read the specified number of bytes from the specified socket into the specified buffer.
+ * If an error occurs during this reading, return -1 and set errno to indicate
+ * the cause of the error. If the read succeeds in reading the specified number of bytes,
+ * return 0. If an EOF occurs before reading the specified number of bytes, return 1.
+ * This function repeats the read attempt until the specified number of bytes
+ * have been read, an EOF is read, or an error occurs. Specifically, errors EAGAIN,
+ * EWOULDBLOCK, and EINTR are not considered errors and instead trigger
+ * another attempt. A delay between attempts is given by DELAY_BETWEEN_SOCKET_RETRIES.
+ * @param socket The socket ID.
+ * @param num_bytes The number of bytes to read.
+ * @param buffer The buffer into which to put the bytes.
+ * @return 0 for success, 1 for EOF, and -1 for an error.
+ */
+ssize_t read_from_netdrv(netdrv_t* netdrv, unsigned char* buffer, size_t buffer_length);
+
+/**
+ * Read the specified number of bytes to the specified socket using read_from_socket
+ * and close the socket if an error occurs. If an error occurs, this will change the
+ * socket ID pointed to by the first argument to -1 and will return -1.
+ * @param socket Pointer to the socket ID.
+ * @param num_bytes The number of bytes to write.
+ * @param buffer The buffer from which to get the bytes.
+ * @return 0 for success, -1 for failure.
+ */
+ssize_t read_from_netdrv_close_on_error(netdrv_t* drv, unsigned char* buffer, size_t buffer_length);
+
+/**
+ * Read the specified number of bytes from the specified socket into the
+ * specified buffer. If a disconnect or an EOF occurs during this
+ * reading, then if format is non-null, report an error and exit.
+ * If the mutex argument is non-NULL, release the mutex before exiting.
+ * If format is null, then report the error, but do not exit.
+ * This function takes a formatted string and additional optional arguments
+ * similar to printf(format, ...) that is appended to the error messages.
+ * @param socket The socket ID.
+ * @param num_bytes The number of bytes to read.
+ * @param buffer The buffer into which to put the bytes.
+ * @param format A printf-style format string, followed by arguments to
+ *  fill the string, or NULL to not exit with an error message.
+ * @return The number of bytes read, or 0 if an EOF is received, or
+ *  a negative number for an error.
+ */
+void read_from_netdrv_fail_on_error(netdrv_t* drv, unsigned char* buffer, size_t buffer_length, lf_mutex_t* mutex,
+                                    char* format, ...);
+
+#endif // LF_SOCKET_SUPPORT_H
