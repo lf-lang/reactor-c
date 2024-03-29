2f009ef53dd10a7cbf9b9e8dae50159a3ae2e53d
Chanhee Lee <chanijjani@gmail.com>
2024-03-28 20:21:03 -0700

    Enable MQTT build

--- core/federated/network/lf_mqtt_support.c
+++ core/federated/network/lf_mqtt_support.c
@@ -1,18 +1,18 @@
-#include <netinet/in.h>  // IPPROTO_TCP, IPPROTO_UDP
+#include <netinet/in.h> // IPPROTO_TCP, IPPROTO_UDP
 
 #include "netdriver.h"
 
 /**
- * @brief 
+ * @brief
  * Initializes structure of netdrv, and priv inside.
  * Allocate memory.
  * Check lf_socket_support.c for example.
- * 
- * @return netdrv_t* 
+ *
+ * @return netdrv_t*
  */
 netdrv_t* netdrv_init() {
-    // FIXME: Delete below.
-    printf("\n\t[MQTT PROTOCOL]\n\n");
+  // FIXME: Delete below.
+  printf("\n\t[MQTT PROTOCOL]\n\n");
 }
 
 char* get_host_name(netdrv_t* drv) {}
@@ -33,32 +33,32 @@ void set_ip_addr(netdrv_t* drv, struct in_addr ip_addr) {}
  * MQTTClient_connect()
  * RTI subscribes “{Federation_ID}_RTI” topic.
  * Check socket_common.c for example. It is a common function because also lf_sst_support.c will also use it.
- * @param drv 
- * @param server_type 
+ * @param drv
+ * @param server_type
  * @param port The port is NULL here.
- * @return int 
+ * @return int
  */
-int create_server(netdrv_t* drv, server_type_t server_type, uint16_t port) { }
+int create_server(netdrv_t* drv, server_type_t server_type, uint16_t port) {}
 
 /**
- * @brief 
+ * @brief
  * 1. Each federate publishes fed_id to {Federation_ID}_RTI
  * 2. fed_{n} subscribes to “{Federation_Id}_RTI_to_fed_{n}”.
  * 3. RTI subscribes to “{Federation_Id}_fed_{n}_to_RTI”.
  * Check lf_socket_support.c for example.
 
- * @param netdrv 
- * @return netdrv_t* 
+ * @param netdrv
+ * @return netdrv_t*
  */
 netdrv_t* establish_communication_session(netdrv_t* netdrv) {}
 
 /**
  * @brief Publish message.
- * 
- * @param drv 
- * @param num_bytes 
- * @param buffer 
- * @return int 
+ *
+ * @param drv
+ * @param num_bytes
+ * @param buffer
+ * @return int
  */
 int write_to_netdrv(netdrv_t* drv, size_t num_bytes, unsigned char* buffer) {}
 
@@ -73,3 +73,5 @@ ssize_t read_from_netdrv_close_on_error(netdrv_t* drv, unsigned char* buffer, si
 
 void read_from_netdrv_fail_on_error(netdrv_t* drv, unsigned char* buffer, size_t buffer_length, lf_mutex_t* mutex,
                                     char* format, ...) {}
+
+int create_clock_sync_server(uint16_t* clock_sync_port) {}
