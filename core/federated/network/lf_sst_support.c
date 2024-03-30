aae595be28b9cd44a0d0364ae114c3fb2de4b147
Dongha Kim <dongha@asu.edu>
2024-03-29 18:43:01 -0700

    Add create_server and establish_communication_session for SST

--- core/federated/network/lf_sst_support.c
+++ core/federated/network/lf_sst_support.c
@@ -4,29 +4,29 @@
 #include "netdriver.h"
 
 static sst_priv_t* sst_priv_init() {
-  sst_priv_t* priv = malloc(sizeof(sst_priv_t));
-  if (!priv) {
+  sst_priv_t* sst_priv = malloc(sizeof(sst_priv_t));
+  if (!sst_priv) {
     lf_print_error_and_exit("Falied to malloc sst_priv_t.");
   }
-  memset(priv, 0, sizeof(sst_priv_t));
-  priv->socket_priv = TCP_socket_priv_init();
-  return priv;
+  memset(sst_priv, 0, sizeof(sst_priv_t));
+  sst_priv->socket_priv = TCP_socket_priv_init();
+  return sst_priv;
 }
 
-static void sst_open(netdrv_t* drv) { sst_priv_t* priv = (sst_priv_t*)drv->priv; }
+static void sst_open(netdrv_t* drv) { sst_priv_t* sst_priv = (sst_priv_t*)drv->priv; }
 static void sst_close(netdrv_t* drv) {
-  sst_priv_t* priv = (sst_priv_t*)drv->priv;
-  if (priv->socket_priv != NULL) {
-    TCP_socket_close(priv->socket_priv);
+  sst_priv_t* sst_priv = (sst_priv_t*)drv->priv;
+  if (sst_priv->socket_priv != NULL) {
+    TCP_socket_close(sst_priv->socket_priv);
   } else {
     lf_print_error("Trying to close TCP socket not existing.");
   }
 }
 
 void netdrv_free(netdrv_t* drv) {
-  sst_priv_t* priv = (sst_priv_t*)drv->priv;
+  sst_priv_t* sst_priv = (sst_priv_t*)drv->priv;
   // free(priv); // Already freed on socket close()
-  free_SST_ctx_t(priv->sst_ctx);
+  free_SST_ctx_t(sst_priv->sst_ctx);
   free(drv);
 }
 
@@ -43,22 +43,63 @@ netdrv_t* netdrv_init() {
   drv->read_remaining_bytes = 0;
 
   // Initialize priv.
-  sst_priv_t* priv = sst_priv_init();
+  sst_priv_t* sst_priv = sst_priv_init();
 
   // Set drv->priv pointer to point the malloc'd priv.
-  drv->priv = (void*)priv;
+  drv->priv = (void*)sst_priv;
   return drv;
 }
 
-// Port will be NULL on MQTT.
-int create_server(netdrv_t* drv, server_type_t server_type, uint16_t port) {
-  sst_priv_t* priv = (sst_priv_t*)drv->priv;
+char* get_host_name(netdrv_t* drv) {
+  sst_priv_t* sst_priv = (sst_priv_t*)drv->priv;
+  return sst_priv->socket_priv->server_hostname;
+}
+int32_t get_my_port(netdrv_t* drv) {
+  sst_priv_t* sst_priv = (sst_priv_t*)drv->priv;
+  return sst_priv->socket_priv->port;
+}
+int32_t get_port(netdrv_t* drv) {
+  sst_priv_t* sst_priv = (sst_priv_t*)drv->priv;
+  return (sst_priv->socket_priv == NULL) ? -1 : sst_priv->socket_priv->server_port;
+}
+//
+struct in_addr* get_ip_addr(netdrv_t* drv) {
+  sst_priv_t* sst_priv = (sst_priv_t*)drv->priv;
+  return &sst_priv->socket_priv->server_ip_addr;
+}
+void set_host_name(netdrv_t* drv, const char* hostname) {
+  sst_priv_t* sst_priv = (sst_priv_t*)drv->priv;
+  memcpy(sst_priv->socket_priv->server_hostname, hostname, INET_ADDRSTRLEN);
+}
+void set_port(netdrv_t* drv, int port) {
+  sst_priv_t* sst_priv = (sst_priv_t*)drv->priv;
+  sst_priv->socket_priv->server_port = port;
 }
 
-// Returns socket number of clock_sync_server.
-int create_clock_sync_server(uint16_t* clock_sync_port) {}
+// Unused.
+void set_ip_addr(netdrv_t* drv, struct in_addr ip_addr) {
+  sst_priv_t* sst_priv = (sst_priv_t*)drv->priv;
+  sst_priv->socket_priv->server_ip_addr = ip_addr;
+}
 
-netdrv_t* establish_communication_session(netdrv_t* netdrv) {}
+// Port will be NULL on MQTT.
+int create_server(netdrv_t* drv, int server_type, uint16_t port) {
+  sst_priv_t* sst_priv = (sst_priv_t*)drv->priv;
+  SST_ctx_t* ctx = init_SST(
+      "/home/dongha/project/lingua-franca/core/src/main/resources/lib/c/reactor-c/core/federated/RTI/c_client.config");
+  sst_priv->sst_ctx = ctx;
+  return create_TCP_server(sst_priv->socket_priv, server_type, port);
+}
+
+netdrv_t* establish_communication_session(netdrv_t* netdrv) {
+  netdrv_t* ret_netdrv = netdrv_init();
+  sst_priv_t* my_priv = (sst_priv_t*)my_netdrv->priv;
+  sst_priv_t* ret_priv = (sst_priv_t*)ret_netdrv->priv;
+  session_key_list_t *s_key_list = init_empty_session_key_list();
+  SST_session_ctx_t* session_ctx = server_secure_comm_setup(my_priv->sst_ctx, my_priv->socket_priv.socket_descriptor, s_key_list);
+  free_session_key_list_t(s_key_list);
+  ret_priv->session_ctx = session_ctx;
+}
 
 int write_to_netdrv(netdrv_t* drv, size_t num_bytes, unsigned char* buffer) {}
 
