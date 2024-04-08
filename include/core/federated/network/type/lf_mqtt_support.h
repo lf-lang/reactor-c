82a450c4659be505768734dba674f169ee9264b0
Dongha Kim <dongha@asu.edu>
2024-04-08 16:35:51 -0700

    Add address, qos, timeout values.

--- include/core/federated/network/type/lf_mqtt_support.h
+++ include/core/federated/network/type/lf_mqtt_support.h
@@ -3,12 +3,15 @@
 
 #include <MQTTClient.h>
 
+#define ADDRESS     "tcp://mqtt.eclipseprojects.io:1883"
+#define QOS         2
+#define TIMEOUT     10000L
+
 typedef struct MQTT_priv_t {
   MQTTClient client;
   MQTTClient_connectOptions conn_opts; // = MQTTClient_connectOptions_initializer;
   MQTTClient_message pubmsg; //= MQTTClient_message_initializer;
   MQTTClient_deliveryToken token;
-  int QOS;
   const char* topic_name;
 } MQTT_priv_t;
 
