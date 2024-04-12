b3dcbacc135fe89b42e504528d3a97dcb592d880
Ehsan Khodadad <ehkh@dtu.dk>
2024-04-12 17:08:32 +0200

    Support for Patmos platform..

--- low_level_platform/impl/src/lf_atomic_irq.c
+++ low_level_platform/impl/src/lf_atomic_irq.c
@@ -1,4 +1,5 @@
-#if defined(PLATFORM_ARDUINO) || defined(PLATFORM_NRF52) || defined(PLATFORM_ZEPHYR) || defined(PLATFORM_RP2040) || defined(PLATFORM_PATMOS)
+#if defined(PLATFORM_ARDUINO) || defined(PLATFORM_NRF52) || defined(PLATFORM_ZEPHYR) || defined(PLATFORM_RP2040) ||    \
+    defined(PLATFORM_PATMOS)
 /**
  * @author Erling Rennemo Jellum
  * @copyright (c) 2023
