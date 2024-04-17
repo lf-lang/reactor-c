88ae0b350502dad15a0a81c0e142e44841aec255
erlingrj <erling.jellum@gmail.com>
2024-04-17 15:30:24 +0200

    Remove unused import

--- low_level_platform/impl/src/lf_linux_support.c
+++ low_level_platform/impl/src/lf_linux_support.c
@@ -36,7 +36,6 @@ THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #define _GNU_SOURCE // Needed to get access to Linux thread-scheduling API
 #include "platform/lf_linux_support.h"
 #include "low_level_platform.h"
-#include "tag.h"
 
 #include "platform/lf_unix_clock_support.h"
 
