74f4cd1c60f6f99ee514f51d3c82471f952c81fd
erlingrj <erling.jellum@gmail.com>
2024-04-28 15:50:29 +0200

    Add a unit test for the scheduling API

--- test/Tests.cmake
+++ test/Tests.cmake
@@ -1,10 +1,10 @@
 # This adds all tests in the test directory.
 include(CTest)
 
-set(TestLib test-lib)
 set(TEST_DIR ${CMAKE_CURRENT_SOURCE_DIR}/test)
 set(TEST_SUFFIX test.c)  # Files that are tests must have names ending with TEST_SUFFIX.
 set(LF_ROOT ${CMAKE_CURRENT_LIST_DIR}/..)
+set(TEST_MOCK_SRCS ${TEST_DIR}/src_gen_stub.c ${TEST_DIR}/rand_utils.c)
 
 # Add the test files found in DIR to TEST_FILES.
 function(add_test_dir DIR)
@@ -29,67 +29,14 @@ endif(NUMBER_OF_WORKERS)
 # Create executables for each test.
 foreach(FILE ${TEST_FILES})
     string(REGEX REPLACE "[./]" "_" NAME ${FILE})
-    add_executable(${NAME} ${TEST_DIR}/${FILE})
+    add_executable(${NAME} ${TEST_DIR}/${FILE} ${TEST_MOCK_SRCS})
     add_test(NAME ${NAME} COMMAND ${NAME})
+    # This is needed for the tests to use the threading API declared in
+    # low_level_platform.h. Ideally this would not be needed.
+    target_link_libraries(${NAME} PRIVATE lf::low-level-platform-impl)
     target_link_libraries(
-        ${NAME} PUBLIC
-        ${CoreLib} ${Lib} ${TestLib}
+        ${NAME} PRIVATE
+        ${CoreLib} ${Lib}
     )
     target_include_directories(${NAME} PRIVATE ${TEST_DIR})
 endforeach(FILE ${TEST_FILES})
-
-# Add the test for the RTI.
-if (NOT DEFINED LF_SINGLE_THREADED)
-    # Check which system we are running on to select the correct platform support
-    # file and assign the file's path to LF_PLATFORM_FILE
-    # FIXME: This is effectively a second build script for the RTI that we have to maintain. This is code duplication.
-    # FIXME: We should not be reaching into the platform directory and bypassing its CMake build.
-    if(${CMAKE_SYSTEM_NAME} STREQUAL "Linux")
-      set(LF_PLATFORM_FILE ${LF_ROOT}/low_level_platform/impl/src/lf_linux_support.c)
-    elseif(${CMAKE_SYSTEM_NAME} STREQUAL "Darwin")
-      set(LF_PLATFORM_FILE ${LF_ROOT}/low_level_platform/impl/src/lf_macos_support.c)
-    else()
-      message(FATAL_ERROR "Your platform is not supported! RTI supports Linux and MacOS.")
-    endif()
-
-    set(IncludeDir include/core)
-
-    set(RTI_DIR ${CoreLibPath}/federated/RTI)
-    add_executable(
-      rti_common_test
-      ${TEST_DIR}/RTI/rti_common_test.c
-      ${RTI_DIR}/rti_common.c
-      ${RTI_DIR}/rti_remote.c
-      ${CoreLibPath}/tracepoint.c
-      ${LF_PLATFORM_FILE}
-      ${LF_ROOT}/low_level_platform/impl/src/lf_platform_util.c
-      ${LF_ROOT}/low_level_platform/impl/src/lf_atomic_gcc_clang.c
-      ${LF_ROOT}/low_level_platform/impl/src/lf_unix_clock_support.c
-      ${CoreLibPath}/utils/util.c
-      ${CoreLibPath}/tag.c
-      ${CoreLibPath}/clock.c
-      ${CoreLibPath}/federated/network/net_util.c
-      ${CoreLibPath}/utils/pqueue_base.c
-      ${CoreLibPath}/utils/pqueue_tag.c
-      ${CoreLibPath}/utils/pqueue.c
-    )
-    add_test(NAME rti_common_test COMMAND rti_common_test)
-    target_include_directories(rti_common_test PUBLIC ${RTI_DIR})
-    target_include_directories(rti_common_test PUBLIC ${IncludeDir})
-    target_include_directories(rti_common_test PUBLIC ${IncludeDir}/federated)
-    target_include_directories(rti_common_test PUBLIC ${IncludeDir}/modal_models)
-    target_link_libraries(rti_common_test lf::low-level-platform-api)
-    target_link_libraries(rti_common_test lf::logging-api)
-    target_include_directories(rti_common_test PUBLIC ${IncludeDir}/utils)
-    # Set the STANDALONE_RTI flag to include the rti_remote and rti_common.
-    target_compile_definitions(rti_common_test PUBLIC STANDALONE_RTI=1)
-
-    # Set FEDERATED to get federated compilation support
-    target_compile_definitions(rti_common_test PUBLIC FEDERATED=1)
-
-    target_compile_definitions(rti_common_test PUBLIC PLATFORM_${CMAKE_SYSTEM_NAME})
-
-    # Find threads and link to it
-    find_package(Threads REQUIRED)
-    target_link_libraries(rti_common_test Threads::Threads)
-endif()
---
+++ test/multithreaded/scheduling_api_test.c
@@ -0,0 +1,82 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "core/utils/util.h"
+#include "low_level_platform.h"
+
+int main(int argc, char** argv) {
+  int res;
+
+  // Set the CPU Set of the current thread.
+  res = lf_thread_set_cpu(lf_thread_self(), lf_available_cores() - 1);
+  if (res != 0) {
+    lf_print_error_and_exit("lf_thread_set_cpu failed with %d", res);
+  }
+
+  // // Pick SCHED_FIFO
+  {
+    lf_scheduling_policy_t cfg;
+    cfg.policy = LF_SCHED_PRIORITY;
+    cfg.priority = 99;
+    cfg.time_slice = 0;
+    res = lf_thread_set_scheduling_policy(lf_thread_self(), &cfg);
+    if (res != 0) {
+      lf_print_error_and_exit("lf_thread_set_scheduling_policy FIFO failed with %d", res);
+    }
+  }
+
+  // Set SCHED_RR
+  {
+    lf_scheduling_policy_t cfg;
+    cfg.policy = LF_SCHED_TIMESLICE;
+    cfg.priority = 99;
+    cfg.time_slice = 0;
+    res = lf_thread_set_scheduling_policy(lf_thread_self(), &cfg);
+    if (res != 0) {
+      lf_print_error_and_exit("lf_thread_set_scheduling_policy RR failed with %d", res);
+    }
+  }
+
+  // Try illegal priority
+  {
+    lf_scheduling_policy_t cfg;
+    cfg.policy = LF_SCHED_TIMESLICE;
+    cfg.time_slice = 0;
+    cfg.priority = 10000;
+    res = lf_thread_set_scheduling_policy(lf_thread_self(), &cfg);
+    if (res == 0) {
+      lf_print_error_and_exit("lf_thread_set_scheduling_policy should have failed with illegal priority");
+    }
+  }
+
+  // Set the priority
+  res = lf_thread_set_priority(lf_thread_self(), 50);
+  if (res != 0) {
+    lf_print_error_and_exit("lf_thread_set_priority failed with %d", res);
+  }
+
+  res = lf_thread_set_priority(lf_thread_self(), -50);
+  if (res == 0) {
+    lf_print_error_and_exit("lf_thread_set_priority should have failed for -50");
+  }
+
+  {
+    lf_scheduling_policy_t cfg;
+    cfg.policy = LF_SCHED_FAIR;
+    res = lf_thread_set_scheduling_policy(lf_thread_self(), &cfg);
+    if (res != 0) {
+      lf_print_error_and_exit("lf_thread_set_scheduling_policy RR failed with %d", res);
+    }
+  }
+
+  // Try to high CPU
+  res = lf_thread_set_cpu(lf_thread_self(), lf_available_cores());
+  if (res == 0) {
+    lf_print_error_and_exit("lf_thread_set_cpu should fail for too high CPU id");
+  }
+
+  res = lf_thread_set_cpu(lf_thread_self(), -1);
+  if (res == 0) {
+    lf_print_error_and_exit("lf_thread_set_cpu should fail for too low CPU id");
+  }
+}
