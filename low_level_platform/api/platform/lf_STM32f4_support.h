d11f3ff254ba00846545304de43557d75810ceb2
naichenzhao <naichenzhao13@gmail.com>
2024-08-04 20:38:29 -0700

    rebased in main

---
+++ low_level_platform/api/platform/lf_STM32f4_support.h
@@ -0,0 +1,27 @@
+/* STM32 API support for the C target of Lingua Franca. */
+
+#ifndef LF_STM32F4_SUPPORT_H
+#define LF_STM32F4_SUPPORT_H
+
+// I have no idea what the fuck TTY is so i guess we dont support it
+#define NO_TTY
+
+// #include <../../../STM_Core/Inc/main.h>
+// #include <../../../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h>
+#include <stm32f4xx_hal.h>
+
+// src-gen/Main/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h
+
+// Defines for formatting time in printf for pico
+#define PRINTF_TAG "(" PRINTF_TIME ", " PRINTF_MICROSTEP ")"
+#define PRINTF_TIME "%lld"
+#define PRINTF_MICROSTEP "%d"
+
+#define LF_TIME_BUFFER_LENGTH 80
+#define _LF_TIMEOUT 1
+
+#ifdef LF_THREADED
+#error "I have no idea how to support threading"
+#endif // LF_THREADED
+
+#endif
