2f4a1087924082ae9233d482dafcd5a6fce1fc4a
Edward A. Lee <eal@eecs.berkeley.edu>
2024-08-28 09:55:13 -0400

    Update core/federated/federate.c
    Co-authored-by: Marten Lohstroh <marten@berkeley.edu>

--- core/federated/federate.c
+++ core/federated/federate.c
@@ -292,7 +292,7 @@ static void update_last_known_status_on_input_port(environment_t* env, tag_t tag
 }
 
 /**
- * @brief Mark all the input ports from this federate as known to be absent until FOREVER.
+ * @brief Mark all the input ports connected to the given federate as known to be absent until FOREVER.
  *
  * This does nothing if the federate is not using decentralized coordination.
  * This function acquires the mutex on the top-level environment.
