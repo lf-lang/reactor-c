400edb8aeec3a73e555b8a5af2914d2a58db3db7
erlingrj <erling.jellum@gmail.com>
2024-09-20 11:10:37 -0700

    Fix forgotten atomics int32->int changes

--- low_level_platform/impl/src/lf_atomic_gcc_clang.c
+++ low_level_platform/impl/src/lf_atomic_gcc_clang.c
@@ -11,9 +11,9 @@
 #include "platform/lf_atomic.h"
 #include "low_level_platform.h"
 
-int32_t lf_atomic_fetch_add(int* ptr, int value) { return __sync_fetch_and_add(ptr, value); }
+int lf_atomic_fetch_add(int* ptr, int value) { return __sync_fetch_and_add(ptr, value); }
 int64_t lf_atomic_fetch_add64(int64_t* ptr, int64_t value) { return __sync_fetch_and_add(ptr, value); }
-int32_t lf_atomic_add_fetch(int* ptr, int value) { return __sync_add_and_fetch(ptr, value); }
+int lf_atomic_add_fetch(int* ptr, int value) { return __sync_add_and_fetch(ptr, value); }
 int64_t lf_atomic_add_fetch64(int64_t* ptr, int64_t value) { return __sync_add_and_fetch(ptr, value); }
 bool lf_atomic_bool_compare_and_swap(int* ptr, int oldval, int newval) {
   return __sync_bool_compare_and_swap(ptr, oldval, newval);
@@ -21,7 +21,7 @@ bool lf_atomic_bool_compare_and_swap(int* ptr, int oldval, int newval) {
 bool lf_atomic_bool_compare_and_swap64(int64_t* ptr, int64_t oldval, int64_t newval) {
   return __sync_bool_compare_and_swap(ptr, oldval, newval);
 }
-int32_t lf_atomic_val_compare_and_swap(int* ptr, int oldval, int newval) {
+int lf_atomic_val_compare_and_swap(int* ptr, int oldval, int newval) {
   return __sync_val_compare_and_swap(ptr, oldval, newval);
 }
 int64_t lf_atomic_val_compare_and_swap64(int64_t* ptr, int64_t oldval, int64_t newval) {
