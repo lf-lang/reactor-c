0031be283fb3bf1cb6561fe0cb2c9604ecbb0ef1
depetrol <lslrobert@outlook.com>
2024-10-11 13:31:58 -0700

    format

--- python/lib/pythontarget.c
+++ python/lib/pythontarget.c
@@ -542,8 +542,8 @@ PyObject* convert_C_action_to_py(void* action) {
   }
 
   // Actions in Python always use token type
-  if ((( generic_action_instance_struct*)action)->token!=NULL)
-  ((generic_action_capsule_struct*)cap)->value = (( generic_action_instance_struct*)action)->token->value;
+  if (((generic_action_instance_struct*)action)->token != NULL)
+    ((generic_action_capsule_struct*)cap)->value = ((generic_action_instance_struct*)action)->token->value;
 
   return cap;
 }
