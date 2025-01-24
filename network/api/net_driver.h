71c83f84692f1e51309a48d5636aac3c0c00d24d
Dongha Kim <dongha@asu.edu>
2025-01-24 12:35:00 -0700

    Remove unnecessary info.

--- network/api/net_driver.h
+++ network/api/net_driver.h
@@ -5,9 +5,6 @@
 
 typedef struct netdrv_t {
   void* priv;
-  //   unsigned int read_remaining_bytes;
-  //   int my_federate_id; // The RTI is -1, and unitialized is -2. This must be int not uint16_t
-  //   const char* federation_id;
 } netdrv_t;
 
 /**
