a814a07e847f28370b21a6ea677c3c9319cb04ab
Dongha Kim <dongha@asu.edu>
2025-01-31 14:59:39 -0700

    Add free_netdrv for sst

--- network/impl/src/lf_sst_support.c
+++ network/impl/src/lf_sst_support.c
@@ -2,6 +2,14 @@
 #include "lf_sst_support.h"
 #include "util.h"
 
+static sst_priv_t* get_sst_priv_t(netdrv_t drv) {
+  if (drv == NULL) {
+    lf_print_error("Network driver is already closed.");
+    return NULL;
+  }
+  return (sst_priv_t*)drv;
+}
+
 netdrv_t initialize_netdrv() {
   // Initialize sst_priv.
   sst_priv_t* sst_priv = malloc(sizeof(sst_priv_t));
@@ -24,10 +32,18 @@ netdrv_t initialize_netdrv() {
   socket_priv->server_ip_addr.s_addr = 0;
   socket_priv->server_port = -1;
 
+  sst_priv->socket_priv = socket_priv;
+
   // SST initialization. Only set pointers to NULL.
   sst_priv->sst_ctx = NULL;
   sst_priv->session_ctx = NULL;
 
   return (netdrv_t)sst_priv;
+}
+
+void free_netdrv(netdrv_t drv) {
+  sst_priv_t* priv = get_sst_priv_t(drv);
+  free(priv->socket_priv);
+  free(priv);
 }
 
