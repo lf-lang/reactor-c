27ac24e66e82f1b18793786c2aec85cc724b0a16
Dongha Kim <dongha@asu.edu>
2025-01-31 14:55:47 -0700

    Add initialize_netdrv for sst.

--- network/impl/src/lf_sst_support.c
+++ network/impl/src/lf_sst_support.c
@@ -1,1 +1,33 @@
 #include "net_driver.h"
+#include "lf_sst_support.h"
+#include "util.h"
+
+netdrv_t initialize_netdrv() {
+  // Initialize sst_priv.
+  sst_priv_t* sst_priv = malloc(sizeof(sst_priv_t));
+  if (sst_priv == NULL) {
+    lf_print_error_and_exit("Falied to malloc sst_priv_t.");
+  }
+  // Initialize socket_priv.
+  socket_priv_t* socket_priv = malloc(sizeof(socket_priv_t));
+  if (socket_priv == NULL) {
+    lf_print_error_and_exit("Falied to malloc socket_priv_t.");
+  }
+
+  // Server initialization.
+  socket_priv->port = 0;
+  socket_priv->user_specified_port = 0;
+  socket_priv->socket_descriptor = -1;
+
+  // Federate initialization
+  strncpy(socket_priv->server_hostname, "localhost", INET_ADDRSTRLEN);
+  socket_priv->server_ip_addr.s_addr = 0;
+  socket_priv->server_port = -1;
+
+  // SST initialization. Only set pointers to NULL.
+  sst_priv->sst_ctx = NULL;
+  sst_priv->session_ctx = NULL;
+
+  return (netdrv_t)sst_priv;
+}
+
