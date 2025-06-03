1e5fa695368e081a9d1df74e0c259945511df405
Edward A. Lee <eal@berkeley.edu>
2025-06-03 09:19:00 -0700

    More docs

--- include/core/threaded/scheduler_sync_tag_advance.h
+++ include/core/threaded/scheduler_sync_tag_advance.h
@@ -1,10 +1,11 @@
 /**
- * @file
+ * @file scheduler_sync_tag_advance.h
  * @author Soroush Bateni
  * @author Edward A. Lee
  * @author Marten Lohstroh
  *
  * @brief API used to advance tag globally.
+ * @ingroup Internal
  */
 
 #ifndef SCHEDULER_SYNC_TAG_ADVANCE_H
@@ -16,25 +17,30 @@
 #include "scheduler_instance.h"
 
 /**
- * Placeholder for code-generated function that will, in a federated
- * execution, be used to coordinate the advancement of tag. It will notify
- * the runtime infrastructure (RTI) that all reactions at the specified
- * logical tag have completed. This function should be called only while
- * holding the mutex lock.
+ * @brief Placeholder for code-generated function that will, in a federated
+ * execution, be used to coordinate the advancement of tag.
+ * @ingroup Internal
+ *
+ * It will notify the runtime infrastructure (RTI) that all reactions at the
+ * specified logical tag have completed.
+ *
+ * This function should be called only while holding the mutex lock.
+ *
  * @param tag_to_send The tag to send.
  */
 void logical_tag_complete(tag_t tag_to_send);
 
 /**
  * @brief Return true if the worker should stop now; false otherwise.
- *
+ * @ingroup Internal
  * This function assumes the caller holds the mutex lock.
  * @param sched The scheduler instance to check.
  */
 bool should_stop_locked(lf_scheduler_t* sched);
 
 /**
- * @brief Advance the tag to the next tag on the event queue
+ * @brief Advance the tag to the next tag on the event queue.
+ * @ingroup Internal
  *
  * This will also pop events for the newly acquired tag and trigger
  * the enabled reactions using the scheduler.
