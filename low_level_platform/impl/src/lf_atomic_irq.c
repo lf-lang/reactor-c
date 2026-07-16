e2400a8b85000cb6d8fe0764020b16702be83f54
Ehsan Khodadad <ehkh@dtu.dk>
2026-07-16 13:07:41 +0200

    Refactor global lock management for atomic operations on Patmos

--- low_level_platform/impl/src/lf_atomic_irq.c
+++ low_level_platform/impl/src/lf_atomic_irq.c
@@ -20,122 +20,74 @@ int lf_enable_interrupts_nested();
 
 int lf_atomic_fetch_add(int* ptr, int value) {
   lf_disable_interrupts_nested();
-#if defined(PLATFORM_PATMOS) && !defined(LF_SINGLE_THREADED)
-  _lf_patmos_global_lock_acquire();
-#endif
   int res = *ptr;
   *ptr += value;
-#if defined(PLATFORM_PATMOS) && !defined(LF_SINGLE_THREADED)
-  _lf_patmos_global_lock_release();
-#endif
   lf_enable_interrupts_nested();
   return res;
 }
 
 int64_t lf_atomic_fetch_add64(int64_t* ptr, int64_t value) {
   lf_disable_interrupts_nested();
-#if defined(PLATFORM_PATMOS) && !defined(LF_SINGLE_THREADED)
-  _lf_patmos_global_lock_acquire();
-#endif
   int64_t res = *ptr;
   *ptr += value;
-#if defined(PLATFORM_PATMOS) && !defined(LF_SINGLE_THREADED)
-  _lf_patmos_global_lock_release();
-#endif
   lf_enable_interrupts_nested();
   return res;
 }
 
 int lf_atomic_add_fetch(int* ptr, int value) {
   lf_disable_interrupts_nested();
-#if defined(PLATFORM_PATMOS) && !defined(LF_SINGLE_THREADED)
-  _lf_patmos_global_lock_acquire();
-#endif
   int res = *ptr + value;
   *ptr = res;
-#if defined(PLATFORM_PATMOS) && !defined(LF_SINGLE_THREADED)
-  _lf_patmos_global_lock_release();
-#endif
   lf_enable_interrupts_nested();
   return res;
 }
 
 int64_t lf_atomic_add_fetch64(int64_t* ptr, int64_t value) {
   lf_disable_interrupts_nested();
-#if defined(PLATFORM_PATMOS) && !defined(LF_SINGLE_THREADED)
-  _lf_patmos_global_lock_acquire();
-#endif
   int64_t res = *ptr + value;
   *ptr = res;
-#if defined(PLATFORM_PATMOS) && !defined(LF_SINGLE_THREADED)
-  _lf_patmos_global_lock_release();
-#endif
   lf_enable_interrupts_nested();
   return res;
 }
 
 bool lf_atomic_bool_compare_and_swap(int* ptr, int oldval, int newval) {
   lf_disable_interrupts_nested();
-#if defined(PLATFORM_PATMOS) && !defined(LF_SINGLE_THREADED)
-  _lf_patmos_global_lock_acquire();
-#endif
   bool res = false;
   if ((*ptr) == oldval) {
     *ptr = newval;
     res = true;
   }
-#if defined(PLATFORM_PATMOS) && !defined(LF_SINGLE_THREADED)
-  _lf_patmos_global_lock_release();
-#endif
   lf_enable_interrupts_nested();
   return res;
 }
 
 bool lf_atomic_bool_compare_and_swap64(int64_t* ptr, int64_t oldval, int64_t newval) {
   lf_disable_interrupts_nested();
-#if defined(PLATFORM_PATMOS) && !defined(LF_SINGLE_THREADED)
-  _lf_patmos_global_lock_acquire();
-#endif
   bool res = false;
   if ((*ptr) == oldval) {
     *ptr = newval;
     res = true;
   }
-#if defined(PLATFORM_PATMOS) && !defined(LF_SINGLE_THREADED)
-  _lf_patmos_global_lock_release();
-#endif
   lf_enable_interrupts_nested();
   return res;
 }
 
 int lf_atomic_val_compare_and_swap(int* ptr, int oldval, int newval) {
   lf_disable_interrupts_nested();
-#if defined(PLATFORM_PATMOS) && !defined(LF_SINGLE_THREADED)
-  _lf_patmos_global_lock_acquire();
-#endif
   int res = *ptr;
   if ((*ptr) == oldval) {
     *ptr = newval;
   }
-#if defined(PLATFORM_PATMOS) && !defined(LF_SINGLE_THREADED)
-  _lf_patmos_global_lock_release();
-#endif
   lf_enable_interrupts_nested();
   return res;
 }
 
 int64_t lf_atomic_val_compare_and_swap64(int64_t* ptr, int64_t oldval, int64_t newval) {
   lf_disable_interrupts_nested();
-#if defined(PLATFORM_PATMOS) && !defined(LF_SINGLE_THREADED)
-  _lf_patmos_global_lock_acquire();
-#endif
   int64_t res = *ptr;
   if ((*ptr) == oldval) {
     *ptr = newval;
   }
-#if defined(PLATFORM_PATMOS) && !defined(LF_SINGLE_THREADED)
-  _lf_patmos_global_lock_release();
-#endif
   lf_enable_interrupts_nested();
   return res;
 }
