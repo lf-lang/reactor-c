b1aba3fdf7209a3641bf1d857a8878269008b60f
Edward A. Lee <eal@berkeley.edu>
2026-08-22 07:35:44 -0700

    Put units on physical time values

--- util/tracing/visualization/fedsd.py
+++ util/tracing/visualization/fedsd.py
@@ -614,7 +614,7 @@ def write_diagram_body(f, x_coor, actors_names, trace_df, svg_height):
     for index, row in trace_df.iterrows():
         # formatted physical time.
         # FIXME: Using microseconds is hardwired here.
-        physical_time = f'{int(row["physical_time"]/1000):,}'
+        physical_time = f'{int(row["physical_time"]/1000):,}us'
 
         if (row['event'] in non_tagged_messages):
             label = row['event']
